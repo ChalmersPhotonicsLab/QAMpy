ed066daccdf7e6b34d4433232baef40fdd631aa6
Jochen Schroeder <cycomanic@mailbox.org>
2015-12-24 15:22:10 +1100

    "fix generic phase recovery to always shift by pi/M

--- dsp/phaserecovery.py
+++ dsp/phaserecovery.py
@@ -1,5 +1,7 @@
+from __future__ import division, print_function
 import numpy as np
-
+import numexpr as ne
+from . segmentaxis import segment_axis
 
 def viterbiviterbi_gen(N, E, M):
     """Viterbi-Viterbi blind phase recovery for an M-PSK signal"""
@@ -11,8 +13,9 @@ def viterbiviterbi_gen(N, E, M):
     phase_est = np.sum(sa[:L-2*N], axis=1)
     phase_est = np.unwrap(np.angle(phase_est))
     E = E[N:L-N]
-    if M == 4: # QPSK needs pi/4 shift
-        phase_est = phase_est - np.pi
+    #if M == 4: # QPSK needs pi/4 shift
+    # need a shift by pi/M for constellation points to not be on the axis
+    phase_est = phase_est - np.pi
     return E*np.exp(-1.j*phase_est/M)
 
 def viterbiviterbi(N, E):
@@ -25,7 +28,7 @@ def viterbiviterbi_ne(N, E):
     E = E.flatten()
     L = len(E)
     phi = np.angle(E)
-    E_raised = ne.evaluate('exp(1.j*phi)**4')
+    E_raised = ne.evaluate('exp(1.j*phi)**:')
     sa = segment_axis(E_raised, 2*N, 2*N-1)
     phase_est = np.sum(sa[:L-2*N], axis=1)
     phase_est = np.unwrap(np.angle(phase_est))
