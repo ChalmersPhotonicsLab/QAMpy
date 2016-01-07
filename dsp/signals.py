8b97da8f9e22f2a353cf45001e3f69e9cc21e1b2
Jochen Schroeder <cycomanic@mailbox.org>
2016-01-08 00:10:18 +1100

    fix typo

--- dsp/signals.py
+++ dsp/signals.py
@@ -61,7 +61,7 @@ def generateRandomQPSKData(N, snr, carrier_f=0, baudrate=1,
         dataQ = np.random.randint(0, high=2, size=Ntmp)
     data = 2*(dataI+1.j*dataQ-0.5-0.5j)
     data /= np.sqrt(2) # normalise Energy per symbol to 1
-    noise = (np.random.randn(N)+1.j*np.random.randn(Ntmp))/np.sqrt(2) # sqrt(2) because N/2 = sigma
+    noise = (np.random.randn(Ntmp)+1.j*np.random.randn(Ntmp))/np.sqrt(2) # sqrt(2) because N/2 = sigma
     outdata = data+noise*10**(-snr/20) #the 20 here is so we don't have to take the sqrt
     outdata = resample(baudrate, samplingrate, outdata)
     return outdata*np.exp(1.j*np.arange(len(data))*carrier_f), dataI, dataQ
