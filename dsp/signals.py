76ba19c2f0dac6953e7e108b720168907887960c
Jochen Schroeder <cycomanic@mailbox.org>
2016-01-07 22:36:42 +1100

    if we upsample we don't need to generate that many bits

--- dsp/signals.py
+++ dsp/signals.py
@@ -50,17 +50,18 @@ def generateRandomQPSKData(N, snr, carrier_f=0, baudrate=1,
         dataQ: ndarray
             data array used for the quadrature channel
     """
+    Ntmp = N*baudrate/samplingrate # we will upsample later
     if PRBS == True:
         seedI = np.random.randint(0, high=2, size=orderI)
         seedQ = np.random.randint(0, high=2, size=orderQ)
-        dataI = make_prbs_extXOR(np.int(orderI), np.int(N), seedI)
-        dataQ = make_prbs_extXOR(np.int(orderQ), np.int(N), seedQ)
+        dataI = make_prbs_extXOR(np.int(orderI), np.int(Ntmp), seedI)
+        dataQ = make_prbs_extXOR(np.int(orderQ), np.int(Ntmp), seedQ)
     else:
-        dataI = np.random.randint(0, high=2, size=N)
-        dataQ = np.random.randint(0, high=2, size=N)
+        dataI = np.random.randint(0, high=2, size=Ntmp)
+        dataQ = np.random.randint(0, high=2, size=Ntmp)
     data = 2*(dataI+1.j*dataQ-0.5-0.5j)
     data /= np.sqrt(2) # normalise Energy per symbol to 1
-    noise = (np.random.randn(N)+1.j*np.random.randn(N))/np.sqrt(2) # sqrt(2) because N/2 = sigma
+    noise = (np.random.randn(N)+1.j*np.random.randn(Ntmp))/np.sqrt(2) # sqrt(2) because N/2 = sigma
     outdata = data+noise*10**(-snr/20) #the 20 here is so we don't have to take the sqrt
     outdata = resample(baudrate, samplingrate, outdata)
     return outdata*np.exp(1.j*np.arange(len(data))*carrier_f), dataI, dataQ
