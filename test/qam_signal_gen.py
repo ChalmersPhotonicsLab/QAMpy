e046e2042f0e5da08b6b289f122b1bd304a38f37
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-01-08 15:34:56 +0100

    update QAM test to class methods

--- test/qam_signal_gen.py
+++ test/qam_signal_gen.py
@@ -9,14 +9,22 @@ M = 32
 snr = np.arange(5, 25, 1)
 N = 10**5
 ser = []
+ser2 = []
 
 modl = modulation.QAMModulator(M)
 for sr in snr:
-    data_rx, symbols, bits = signals.generate_MQAM_data_signal(N, sr, modl)
-    ser.append(np.count_nonzero(modl.quantize(data_rx)[0]-symbols)/N)
+    data_rx, symbols, bits = modl.generateSignal(N, sr)
+    ser.append(modl.calculate_SER(data_rx, symbol_tx=symbols))
+    ser2.append(modl.calculate_SER(data_rx, bits_tx=bits))
+
+ser = 10*np.log10(np.array(ser))
+ser2 = 10*np.log10(np.array(ser2))
+theory_ser = 10*np.log10(modl.theoretical_SER(10**(snr/10)))
+assert np.allclose(ser, ser2), "SER calculated from symbol is different to ser calculated from bits"
+#assert np.allclose(ser, theory_ser, atol=1), "SER calculated from symbol is different to theoretical ser"
 plt.figure()
-plt.plot(snr, 10*np.log10(theory.MQAM_SERvsEsN0(10**(snr/10.), M)), label='theory')
-plt.plot(snr, 10*np.log10(ser), 'or',label='calculation')
+plt.plot(snr, theory_ser, label='theory')
+plt.plot(snr, ser, 'or',label='calculation')
 plt.xlabel('SNR [dB]')
 plt.ylabel('SER [dB]')
 plt.legend()
