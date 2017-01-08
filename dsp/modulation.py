fix calculation of size for separate IQ modulation

dsp/modulation.py
@@ -241,6 +241,7 @@ class QAMModulator(object):
                 bitsq = make_prbs_extXOR(PRBSorder, Nbits, PRBSseed)
             else:
                 bitsq = np.random.randint(0, high=2, size=Nbits).astype(np.bool)
+        print(bitsq.shape)
         symbols = self.modulate(bitsq)
         noise = (np.random.randn(N) + 1.j * np.random.randn(N)) / np.sqrt(
             2)  # sqrt(2) because N/2 = sigma
@@ -303,9 +304,9 @@ def twostreamPRBS(Nsyms, bits, PRBS=True, PRBSorder=(15, 23), PRBSseed=(None,Non
         1D array of booleans that are the interleaved PRBS sequence
     """
     if bits % 2:
-        Nbits = (Nsyms * bits // 2, Nsyms * bits // 2)
+        Nbits = (Nsyms * (bits // 2 + 1), Nsyms * bits // 2)
     else:
-        Nbits = ((Nsyms + 1) * bits // 2, Nsyms * bits // 2)
+        Nbits = (Nsyms * bits // 2, Nsyms * bits // 2)
     if PRBS:
         bitsq1 = make_prbs_extXOR(PRBSorder[0], Nbits[0], PRBSseed[0])
         bitsq2 = make_prbs_extXOR(PRBSorder[0], Nbits[1], PRBSseed[1])

dsp/utils.py
@@ -357,9 +357,9 @@ def convert_iqtosinglebitstream(idat, qdat, nbits):
         N = [nbits//2+1, nbits//2]
     else:
         N = [nbits//2, nbits//2]
-    idat_n = idat[:-(len(idat)%N[0]+1)]
+    idat_n = idat[:len(idat)-(len(idat)%N[0])]
     idat_n = idat_n.reshape(N[0], len(idat_n)/N[0])
-    qdat_n = qdat[:-(len(idat)%N[1]+1)]
+    qdat_n = qdat[:len(qdat)-(len(qdat)%N[1])]
     qdat_n = qdat_n.reshape(N[1], len(qdat_n)/N[1])
     l = min(len(idat_n[0]), len(qdat_n[0]))
     return np.hstack([idat_n[:l], qdat_n[:l]]).flatten()