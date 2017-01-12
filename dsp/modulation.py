32cf99a2a67ae5cb2f47ea312720f4b30939c96f
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-01-12 12:55:16 +0100

    fix error in scaling factor calculation

--- dsp/modulation.py
+++ dsp/modulation.py
@@ -45,7 +45,7 @@ def calculate_MQAM_scaling_factor(M):
     Calculate the scaling factor for normalising MQAM symbols to 1 average Power
     """
     if not self.bits % 2:
-        scale = theory.MQAMScalingFactor(self.M)
+        scale = theory.MQAMScalingFactor(M)
     else:
         symbols = calculate_MQAM_symbols(M)
         scale = (abs(symbols)**2).mean()
