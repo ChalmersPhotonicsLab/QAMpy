3b1f5b31d2b865bd640e42a60477def1d96f3aca
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-01-16 21:54:15 +0100

    format whitespace in cython file

--- dsp/equalisation/equaliser_cython.pyx
+++ dsp/equalisation/equaliser_cython.pyx
@@ -4,7 +4,8 @@ import numpy as np
 cimport cython
 cimport numpy as np
 
-def partition_value(double signal, np.ndarray[ndim=1, dtype=np.float64_t] partitions,
+def partition_value(double signal,
+                    np.ndarray[ndim=1, dtype=np.float64_t] partitions,
                     np.ndarray[ndim=1, dtype=np.float64_t] codebook):
     cdef unsigned int index = 0
     cdef unsigned int L = len(partitions)
@@ -13,31 +14,39 @@ def partition_value(double signal, np.ndarray[ndim=1, dtype=np.float64_t] partit
     return codebook[index]
 
 def FS_CMA_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                    int TrSyms, int Ntaps, unsigned int os, double mu,  np.ndarray[ndim=2, dtype=np.complex128_t] wx,
+                    int TrSyms,
+                    int Ntaps,
+                    unsigned int os,
+                    double mu,
+                    np.ndarray[ndim=2, dtype=np.complex128_t] wx,
                     double R):
     cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
     cdef np.ndarray[ndim=2, dtype=np.complex128_t] X = np.zeros([2,Ntaps], dtype=np.complex128)
     cdef unsigned int i, j, k
     cdef np.complex128_t Xest
     for i in range(0, TrSyms):
-       Xest = 0.j
+        Xest = 0.j
        for j in range(Ntaps):
            for k in range(2):
                X[<unsigned int> k, <unsigned int> j] = E[<unsigned int> k,
-                       <unsigned int> i*os+j]
+                                                         <unsigned int> i*os+j]
                Xest +=  wx[<unsigned int> k,<unsigned int> j]*X[<unsigned int>
-                       k,<unsigned int> j]
-       err[<unsigned int> i] = (Xest.real**2+Xest.imag**2 - R)*Xest
+                                                                k,<unsigned int> j]
+               err[<unsigned int> i] = (Xest.real**2+Xest.imag**2 - R)*Xest
        for j in range(Ntaps):
            for k in range(2):
                wx[<unsigned int> k,<unsigned int> j] = wx[<unsigned int>
-                       k,<unsigned int> j]-mu*err[<unsigned int>
-                               i]*X[<unsigned int> k,
-                                   <unsigned int> j].conjugate()
+                                                          k,<unsigned int> j]-mu*err[<unsigned int>
+                                                                                     i]*X[<unsigned int> k,
+                                                                                          <unsigned int> j].conjugate()
     return err, wx
 
 def FS_MCMA_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                     int TrSyms, int Ntaps, unsigned int os, double mu,  np.ndarray[ndim=2, dtype=np.complex128_t] wx,
+                     int TrSyms,
+                     int Ntaps,
+                     unsigned int os,
+                     double mu,
+                     np.ndarray[ndim=2, dtype=np.complex128_t] wx,
                      np.complex128_t R):
     cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
     cdef np.ndarray[ndim=2, dtype=np.complex128_t] X = np.zeros([2,Ntaps], dtype=np.complex128)
@@ -61,11 +70,13 @@ def FS_MCMA_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
     return err, wx
 
 def FS_RDE_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-        int TrRDE, int Ntaps, unsigned int os,
-        double muRDE,
-        np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-        np.ndarray[ndim=1, dtype=np.float64_t] partition,
-        np.ndarray[ndim=1, dtype=np.float64_t] codebook):
+                    int TrRDE,
+                    int Ntaps,
+                    unsigned int os,
+                    double muRDE,
+                    np.ndarray[ndim=2, dtype=np.complex128_t] wx,
+                    np.ndarray[ndim=1, dtype=np.float64_t] partition,
+                    np.ndarray[ndim=1, dtype=np.float64_t] codebook):
     cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrRDE, dtype=np.complex128)
     cdef np.ndarray[ndim=2, dtype=np.complex128_t] X = np.zeros([2,Ntaps], dtype=np.complex128)
     cdef unsigned int i, j, k
@@ -91,11 +102,11 @@ def FS_RDE_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
     return err, wx
 
 def FS_MRDE_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-        int TrRDE, int Ntaps, unsigned int os,
-        double muRDE,
-        np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-        np.ndarray[ndim=1, dtype=np.complex128_t] partition,
-        np.ndarray[ndim=1, dtype=np.complex128_t] codebook):
+                     int TrRDE, int Ntaps, unsigned int os,
+                     double muRDE,
+                     np.ndarray[ndim=2, dtype=np.complex128_t] wx,
+                     np.ndarray[ndim=1, dtype=np.complex128_t] partition,
+                     np.ndarray[ndim=1, dtype=np.complex128_t] codebook):
     cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrRDE, dtype=np.complex128)
     cdef np.ndarray[ndim=2, dtype=np.complex128_t] X = np.zeros([2,Ntaps], dtype=np.complex128)
     cdef unsigned int i, j, k
