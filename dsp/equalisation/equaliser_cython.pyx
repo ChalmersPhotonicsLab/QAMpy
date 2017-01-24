05373c4c88c0a81376e90da845585efa4a9e6ce6
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-01-24 08:46:49 +0100

    add adaptive MCMA in cython

--- dsp/equalisation/equaliser_cython.pyx
+++ dsp/equalisation/equaliser_cython.pyx
@@ -57,6 +57,40 @@ def FS_CMA_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
                                                                                           <unsigned int> j].conjugate()
     return err, wx
 
+def MCMA_adaptive(np.ndarray[ndim=2, dtype=np.complex128_t] E,
+                  int TrSyms,
+                  int Ntaps,
+                  unsigned int os,
+                  double mu,
+                  np.ndarray[ndim=2, dtype=np.complex128_t] wx,
+                  np.complex128_t R):
+    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
+    cdef np.ndarray[ndim=2, dtype=np.complex128_t] X = np.zeros([2,Ntaps], dtype=np.complex128)
+    cdef unsigned int i, j, k
+    cdef np.complex128_t Xest
+    for i in range(0, TrSyms):
+        Xest = 0.j
+        for j in range(Ntaps):
+            for k in range(2):
+                X[<unsigned int> k, <unsigned int> j] = E[<unsigned int> k,
+                                                         <unsigned int> i*os+j]
+                Xest +=  wx[<unsigned int> k,<unsigned int> j]*X[<unsigned int>
+                                                                k,<unsigned int> j]
+                err[<unsigned int> i] = (Xest.real**2 - R.real)*Xest.real + 1.j*(Xest.imag**2 - R.imag)*Xest.imag
+        for j in range(Ntaps):
+            for k in range(2):
+                wx[<unsigned int> k,<unsigned int> j] = wx[<unsigned int>
+                                                          k,<unsigned int> j]-mu*err[<unsigned int>
+                                                                                     i]*X[<unsigned int> k,
+                                                                                          <unsigned int> j].conjugate()
+        if i > 0:
+            if err[<unsigned int> i].real*err[<unsigned int> i-1].real > 0 and err[<unsigned int> i].imag*err[<unsigned int> i-1].imag >  0:
+                lm = 0
+            else:
+                lm = 1
+                mu = mu/(1+lm*mu*(err[<unsigned int> i].real*err[<unsigned int> i].real + err[<unsigned int> i].imag*err[<unsigned int> i].imag))
+    return err, wx
+
 def FS_MCMA_training(np.ndarray[ndim=2, dtype=np.complex128_t] E,
                      int TrSyms,
                      int Ntaps,
