ac937a26cb9d25cd0e09f2dad15f4ac95b791ca6
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-01-28 21:55:30 +0100

    clarify theoretical EVM calculation

--- dsp/theory.py
+++ dsp/theory.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
 from scipy.special import erfc
-from .utils import bin2gray
+from .utils import bin2gray, dB2lin
 
 # All the formulas below are taken from dsplog.com
 
@@ -23,15 +23,34 @@ def MQAM_SERvsEsN0(snr, M):
     return 2*(1-1/np.sqrt(M))*erfc(np.sqrt(3*snr/(2*(M-1)))) -\
             (1-2/np.sqrt(M)+1/M)*erfc(np.sqrt(3*snr/(2*(M-1))))**2
 
-def MQAM_BERvsEVM(evm, M):
+def MQAM_BERvsEVM(evm_dB, M):
     """Calculate the bit-error-rate for a M-QAM signal as a function of EVM. Taken from _[1]. Note that here we miss the square in the definition to match the plots given in the paper.
+
+    Parameters
+    ----------
+    evm_dB     : array_like
+          Error-Vector Magnitude in dB
+
+    M          : integer
+          order of M-QAM
+
+    Returns
+    -------
+
+    ber        : array_like
+          bit error rate in  linear units
 
     References
     ----------
     ...[3] Shafik, R. (2006). On the extended relationships among EVM, BER and SNR as performance metrics. In Conference on Electrical and Computer Engineering (p. 408). Retrieved from http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=4178493
+
+    Note
+    ----
+    The EVM in dB is defined as $EVM(dB) = 10 \log_{10}{P_{error}/P_{reference}}$ (see e.g. Wikipedia) this is different to the percentage EVM which is defined as the RMS value i.e. $EVM(\%)=\sqrt{1/N \sum^N_{j=1} [(I_j-I_{j,0})^2 + (Q_j -Q_{j,0})^2]} $ so there is a difference of a power 2. So to get the same plot as in _[1] we need to enter the log of EVM^2
     """
     L = np.sqrt(M)
-    ber = 2*(1-1/L)/np.log2(L)*Q_function(np.sqrt(3*np.log2(L)/(L**2-1)*(2/(evm*np.log2(M)))))
+    evm = dB2lin(evm_dB)
+    ber = 2*(1-1/L)/np.log2(L)*Q_function(np.sqrt(3*np.log2(L)/(L**2-1)*(2/(evm**2*np.log2(M)))))
     return ber
 
 
