4dfcb4ab665da39fd220fc4dc887ff1218988f6e
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-02-04 19:45:57 +0100

    add adaptive step option to numba functions

--- dsp/equalisation/training_python.py
+++ dsp/equalisation/training_python.py
@@ -30,6 +30,14 @@ def partition_signal(signal, partitions, codebook):
         quanta.append(codebook[index])
     return quanta
 
+def adapt_step(mu, err_p, err):
+    if err.real*err_p.real > 0 and err.imag*err_p.imag >  0:
+        lm = 0
+    else:
+        lm = 1
+    mu = mu/(1+lm*mu*(err.real*err.real + err.imag*err.imag))
+    return mu
+
 @numba.jit(nopython=True)
 def partition_value(signal, partitions, codebook):
     """
@@ -66,62 +74,6 @@ def sum(x):
     return __sum
 
 @numba.jit(nopython=True)
-def FS_MCMA_adaptive(E, TrSyms, Ntaps, os, mu, wx, R):
-    """
-    Modified CMA algorithm with adaptive step size to determine the equaliser taps. Details in _[1]. Assumes a normalised signal.
-
-    Parameters
-    ----------
-    E       : array_like
-       dual polarisation signal field
-
-    TrSyms : int
-       number of symbols to use needs to be less than len(Ex)
-
-    Ntaps   : int
-       number of equaliser taps
-
-    os      : int
-       oversampling factor
-
-    mu   : float
-       step size parameter
-
-    wx     : array_like
-       initial equaliser taps
-
-    R      : complex
-       CMA cost constant, the real part applies to the real error the imaginary to the imaginary
-    
-    Returns
-    -------
-
-    err       : array_like
-       estimation error for x and y polarisation
-
-    wx    : array_like
-       equaliser taps
-
-    Notes
-    -----
-    ..[1] D. Ashmawy, K. Banovic, E. Abdel-Raheem, M. Youssif, H. Mansour, and M. Mohanna, “Joint MCMA and DD blind equalization algorithm with variable-step size,” Proc. 2009 IEEE Int. Conf. Electro/Information Technol. EIT 2009, no. 1, pp. 174–177, 2009.
-    """
-    err = np.zeros(TrSyms, dtype=np.complex128)
-    lm =1
-    for i in range(TrSyms):
-        X = E[:, i * os:i * os + Ntaps]
-        Xest = sum(wx * X)
-        err[i] = (np.abs(Xest.real)**2 - R.real) * Xest.real + (np.abs(Xest.imag)**2 - R.imag)*Xest.imag*1.j
-        if i > 0:
-            if err[i].real*err[i-1].real>0 and err[i].imag*err[i-1].imag>0:
-                lm = 0
-            else:
-                lm = 1
-        wx -= mu * err[i] * np.conj(X)
-        mu = mu/(1+lm*mu*abs(err[i])**2)
-    return err, wx
-
-@numba.jit(nopython=True)
 def FS_MCMA_adaptive2(E, TrSyms, Ntaps, os, mu, wx, R, syms, R2):
     err = np.zeros(TrSyms, dtype=np.complex128)
     counter1 = 0
@@ -137,7 +89,7 @@ def FS_MCMA_adaptive2(E, TrSyms, Ntaps, os, mu, wx, R, syms, R2):
     return err, wx, mu, counter1
 
 @numba.jit(nopython=True)
-def FS_MCMA(E, TrSyms, Ntaps, os, mu, wx, R):
+def FS_MCMA(E, TrSyms, Ntaps, os, mu, wx, R, adaptive=False):
     """
     Training of the Modified CMA algorithm to determine the equaliser taps. Details in _[1]. Assumes a normalised signal.
 
@@ -184,10 +136,12 @@ def FS_MCMA(E, TrSyms, Ntaps, os, mu, wx, R):
         Xest = sum(wx*X)
         err[i] = (np.abs(Xest.real)**2 - R.real) * Xest.real + (np.abs(Xest.imag)**2 - R.imag)*Xest.imag*1.j
         wx -= mu * err[i] * np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
 @numba.jit(nopython=True)
-def FS_CMA(E, TrSyms, Ntaps, os, mu, wx, R):
+def FS_CMA(E, TrSyms, Ntaps, os, mu, wx, R, adaptive=False):
     """
     Training of the CMA algorithm to determine the equaliser taps.
 
@@ -229,10 +183,12 @@ def FS_CMA(E, TrSyms, Ntaps, os, mu, wx, R):
         Xest = np.sum(wx * X)
         err[i] = (abs(Xest)**2 - R)*Xest
         wx -= mu * err[i] *  np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
 @numba.jit(nopython=True)
-def FS_MRDE(E, TrSyms, Ntaps, os, mu, wx, part, code):
+def FS_MRDE(E, TrSyms, Ntaps, os, mu, wx, part, code, adaptive=False):
     """
     Training of the Modified RDE algorithm to determine the equaliser taps. Details in _[1]. Assumes a normalised signal.
 
@@ -283,10 +239,12 @@ def FS_MRDE(E, TrSyms, Ntaps, os, mu, wx, part, code):
         R = partition_value(Ssq.real, part.real, code.real) + partition_value(Ssq.imag, part.imag, code.imag)*1.j
         err[i] = (Ssq.real - R.real)*Xest.real + (Ssq.imag - R.imag)*1.j*Xest.imag
         wx -= mu * err[i] * np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
 @numba.jit(nopython=True)
-def FS_SBD(E, TrSyms, Ntaps, os, mu, wx, symbols):
+def FS_SBD(E, TrSyms, Ntaps, os, mu, wx, symbols, adaptive=False):
     """
     Symbol Based Decision (SBD) training function after _[1]. This is a DD error function. This does not implement the neighbor weigthing detailed further in _[1].
 
@@ -333,10 +291,12 @@ def FS_SBD(E, TrSyms, Ntaps, os, mu, wx, symbols):
         R = symbols[np.argmin(np.abs(Xest-symbols))]
         err[i] = (Xest.real - R.real)*abs(R.real) + (Xest.imag - R.imag)*1.j*abs(R.imag)
         wx -= mu * err[i] * np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
 @numba.jit(nopython=True)
-def FS_MDDMA(E, TrSyms, Ntaps, os, mu, wx, symbols):
+def FS_MDDMA(E, TrSyms, Ntaps, os, mu, wx, symbols, adaptive=False):
     """
     Modified Decision Directed Modulus Algorithm (MDDMA) after _[1].
     This is a DD error function. 
@@ -384,10 +344,12 @@ def FS_MDDMA(E, TrSyms, Ntaps, os, mu, wx, symbols):
         R = symbols[np.argmin(np.abs(Xest-symbols))]
         err[i] = (Xest.real**2 - R.real**2)*Xest.real + (Xest.imag**2 - R.imag**2)*1.j*Xest.imag
         wx -= mu * err[i] * np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
 @numba.jit(nopython=True)
-def FS_RDE(E, TrSyms, Ntaps, os, mu, wx, part, code):
+def FS_RDE(E, TrSyms, Ntaps, os, mu, wx, part, code, adaptive=False):
     """
     Training of the RDE algorithm to determine the equaliser taps.
 
@@ -434,10 +396,12 @@ def FS_RDE(E, TrSyms, Ntaps, os, mu, wx, part, code):
         S_DD = partition_value(Ssq, part, code)
         err[i] = (Ssq - S_DD)*Xest
         wx -= mu * err[i] * np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
 @numba.jit(nopython=True)
-def FS_CME(E, TrSyms, Ntaps, os, mu, wx, R, d, beta):
+def FS_CME(E, TrSyms, Ntaps, os, mu, wx, R, d, beta, adaptive=False):
     """Constellation matched error algorithm after _[1]
 
     References
@@ -451,10 +415,12 @@ def FS_CME(E, TrSyms, Ntaps, os, mu, wx, R, d, beta):
         err[i] = (abs(Xest)**2 - R)*Xest
         err[i] +=  beta * np.pi/(2*d) * (np.sin(Xest.real*np.pi/d) + 1.j * np.sin(Xest.imag*np.pi/d)) 
         wx -= mu * err[i] *  np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
 @numba.jit(nopython=True)
-def FS_SCA(E, TrSyms, Ntaps, os, mu, wx, R):
+def FS_SCA(E, TrSyms, Ntaps, os, mu, wx, R, adaptive=False):
     """Constellation matched error algorithm after _[1]
 
     References
@@ -476,5 +442,7 @@ def FS_SCA(E, TrSyms, Ntaps, os, mu, wx, R):
             B = 1
         err[i] = 4*Xest.real*(4*Xest.real**2 - 4*R**2)*A + 1.j*4*Xest.imag*(4*Xest.imag**2 - 4*R**2)*B
         wx -= mu * err[i] *  np.conj(X)
+        if adaptive and i > 0:
+            mu = adapt_step(mu, err[i], err[i-1])
     return err, wx
 
