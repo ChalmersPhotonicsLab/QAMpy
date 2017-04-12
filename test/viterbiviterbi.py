2dbffc63f53fb37c524dd68591019ab58275a04f
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-04-12 15:22:27 +0200

    fix viterbi test

--- test/viterbiviterbi.py
+++ test/viterbiviterbi.py
@@ -1,25 +1,25 @@
 import numpy as np
 import matplotlib.pylab as plt
-from dsp import signals, phaserecovery
-from dsp.signal_quality import cal_blind_evm
+from dsp import signals, phaserecovery, modulation, utils
 
 
 fb = 40.e9
 os = 2
 fs = os*fb
-N = 2**18
+N = 10**5
 theta = np.pi/2.45
+M = 4
+snr = 14
 carrier_f = 1e6
+QAM = modulation.QAMModulator(M)
+X, Sx, Bx = QAM.generateSignal(N, snr, baudrate=fb, samplingrate=fs , carrier_df=carrier_f)
 
-X, XIdata, XQdata = signals.generateRandomQPSKData(N, 14, baudrate=fb, samplingrate=fs, carrier_df=carrier_f)
 
-Y = X * np.exp(-1.j*2*np.pi*np.arange(len(X))*carrier_f/fs)
-Y = Y[::2]
 
 E = phaserecovery.viterbiviterbi_qpsk(9, X[::2])
 
-evm1 = cal_blind_evm(E, 4)
-evm2 = cal_blind_evm(Y, 4)
+evm1 = QAM.cal_EVM(E)
+evm2 = QAM.cal_EVM(X)
 
 plt.figure()
 plt.subplot(121)
@@ -28,6 +28,6 @@ plt.plot(E.real, E.imag, 'ro', label=r"$EVM=%.1f\%%$"%(100*evm1))
 plt.legend()
 plt.subplot(122)
 plt.title('Original')
-plt.plot(Y.real, Y.imag, 'go', label=r"$EVM_y=%.1f\%%$"%(100*evm2))
+plt.plot(X.real, X.imag, 'go', label=r"$EVM_y=%.1f\%%$"%(100*evm2))
 plt.legend()
 plt.show()
