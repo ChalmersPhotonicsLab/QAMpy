091ebbbde4c9c151f3f411c59f951b9114079d9e
mazurm@chalmers.se <mazurm@chalmers.se>
2017-05-27 11:59:53 +0200

    Starting making script for generating a symbol sequence with pilots inserted

---
+++ dsp/pilotbased_transmitter.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 20:31:05 2017
+
+Pilot-Based DSP Transmitter for MQAM with M>4
+
+@author: mazurm
+"""
+import numpy as np
+from dsp import signals, equalisation, modulation, utils, phaserecovery, dsp_cython, signal_quality
+from scipy.io import loadmat, savemat
+from .prbs import make_prbs_extXOR
+
+# Oversampling
+os = 2
+    
+# Mapper settings
+M = 128
+
+# Total frame length
+frame_length = 2**18
+
+# Initial number of pilot tones for equalizer pre-convergence
+pilot_sequence_length = 256
+
+# Repetative pilot symbols for phase tracking and equalizer update
+pilot_insertion_ratio = 32 
+
+# Settings if PRBS is seleted for generation
+PRBS = False
+PRBSorder=15,
+PRBSseed=None
+
+data_modulator = modulation.QAMModulator(M)
+pilot_modualtor = modulation.QAMModulator(4)
+
+
+N_pilot_symbs = (frame_length - pilot_sequence_length) / pilot_insertion_ratio
+
+if (N_pilot_symbs%1) != 0:
+    print("Pilot insertion ratio not propper selected")
+
+N_data_symbs = frame_length - N_pilot_symbs
+
+N_pilot_bits = N_pilot_symbs * pilot_modualtor.bits
+N_data_bits = N_data_symbs * data_modulator.bits
+
+# Generate bits for modulation
+if PRBS == True:
+    pilot_bits = make_prbs_extXOR(PRBSorder, N_pilot_bits, PRBSseed)
+    data_bits = make_prbs_extXOR(PRBSorder, N_data_bits, PRBSseed)
+else:
+    pilot_bits = np.random.randint(0, high=2, size=N_pilot_bits).astype(np.bool)    
+    data_bits = np.random.randint(0, high=2, size=N_data_bits).astype(np.bool)
+
+
+
+pilot_symbols = pilot_modulator.modulate(pilot_bits)
+data_symbols = data_modulator.modulate(data_bits)
+ 
+
