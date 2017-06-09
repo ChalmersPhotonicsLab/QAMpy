1aec87fd6eced80db99a8256ee486934a9c05154
mazurm@chalmers.se <mazurm@chalmers.se>
2017-06-09 16:36:54 +0200

    Updated the transmitter to handle multiple modes

--- dsp/pilotbased_transmitter.py
+++ dsp/pilotbased_transmitter.py
@@ -16,7 +16,7 @@ import matplotlib.pylab as plt
 
 
 
-def gen_dataframe_withpilots(M,frame_length = 2**18, pilot_seq_len = 256, pilot_ins_ratio = 32, PRBS = False, PRBSorder=15, PRBSseed=None):
+def gen_dataframe_withpilots(M,npols, frame_length = 2**18, pilot_seq_len = 256, pilot_ins_ratio = 32, PRBS = False, PRBSorder=15, PRBSseed=None):
 
     data_modulator = modulation.QAMModulator(M)
     pilot_modualtor = modulation.QAMModulator(4)
@@ -33,58 +33,71 @@ def gen_dataframe_withpilots(M,frame_length = 2**18, pilot_seq_len = 256, pilot_
     N_pilot_bits = (N_pilot_symbs) * pilot_modualtor.bits
     N_data_bits = N_data_symbs * data_modulator.bits
     
-    # Generate bits for modulation
-    if PRBS == True:
-        pilot_bits = make_prbs_extXOR(PRBSorder, N_pilot_bits, PRBSseed)
-        data_bits = make_prbs_extXOR(PRBSorder, N_data_bits, PRBSseed)
-    else:
-        pilot_bits = np.random.randint(0, high=2, size=N_pilot_bits).astype(np.bool)    
-        data_bits = np.random.randint(0, high=2, size=N_data_bits).astype(np.bool)
     
-    pilot_symbs = pilot_modualtor.modulate(pilot_bits)
-    data_symbs = data_modulator.modulate(data_bits)
-     
+        # Set sequence together
+    symbol_seq = np.zeros([npols,frame_length], dtype = complex)
+    data_symbs = np.zeros([npols,N_data_symbs], dtype = complex)
+    pilot_symbs = np.zeros([npols,N_pilot_symbs], dtype = complex)
     
-    # Set sequence together
-    symbol_seq = np.zeros(frame_length, dtype=np.complex)
+    for l in range(npols):
     
-    # Insert pilot sequence
-    symbol_seq[0:pilot_seq_len] = pilot_symbs[0:pilot_seq_len]
-    symbol_seq\
-    [(pilot_seq_len)::pilot_ins_ratio] \
-    = pilot_symbs[pilot_seq_len:]
-    
-    # Insert data symbols
-    for j in np.arange(N_data_frames):
-        symbol_seq[pilot_seq_len + j*pilot_ins_ratio + 1:\
-                        pilot_seq_len + (j+1)*pilot_ins_ratio ] = \
-                        data_symbs[j*(pilot_ins_ratio-1):(j+1)*(pilot_ins_ratio-1)]
+        # Generate bits for modulation
+        if PRBS == True:
+            pilot_bits = make_prbs_extXOR(PRBSorder, N_pilot_bits, PRBSseed)
+            data_bits = make_prbs_extXOR(PRBSorder, N_data_bits, PRBSseed)
+        else:
+            pilot_bits = np.random.randint(0, high=2, size=N_pilot_bits).astype(np.bool)    
+            data_bits = np.random.randint(0, high=2, size=N_data_bits).astype(np.bool)
+        
+        pilot_symbs[l,:] = pilot_modualtor.modulate(pilot_bits)
+        data_symbs[l,:] = data_modulator.modulate(data_bits)
+
+        # Insert pilot sequence
+        symbol_seq[l,0:pilot_seq_len] = pilot_symbs[l,0:pilot_seq_len]
+        symbol_seq\
+        [l,(pilot_seq_len)::pilot_ins_ratio] \
+        = pilot_symbs[l,pilot_seq_len:]
+        
+        # Insert data symbols
+        for j in np.arange(N_data_frames):
+            symbol_seq[l,pilot_seq_len + j*pilot_ins_ratio + 1:\
+                            pilot_seq_len + (j+1)*pilot_ins_ratio ] = \
+                            data_symbs[l,j*(pilot_ins_ratio-1):(j+1)*(pilot_ins_ratio-1)]
     
     return symbol_seq, data_symbs, pilot_symbs
+
 
 def sim_tx(frame, os, symb_rate = 20e9, beta = 0.1, snr = None, linewidth = None, freqoff = None):
     """
     Function to simulate transmission distortions to pilot frame
     
     """
-    # Upsample and pulse shaping
-    if os > 1:
-        sig = utils.rrcos_resample_zeroins(frame, 1, os, beta = beta, renormalise = True)
     
-    # Add AWGN
-    if snr is not None:
-        sig = utils.add_awgn(sig,10**(-snr/20)*np.sqrt(os))
+    npols = frame.shape[0]
+    sig = np.zeros([npols,len(frame[0,:])*os],dtype = complex)
     
-    # Add Phase Noise
-    if linewidth is not None:       
-        sig = sig*np.exp(1j*utils.phase_noise(sig.shape, linewidth, symb_rate * os ))
+    for l in range(npols):
     
-    # Add FOE
-    if freqoff is not None:
-        sig *= np.exp(2.j * np.pi * np.arange(len(sig)) * fo / (symb_rate * os))
+        # Upsample and pulse shaping
+        if os > 1:
+            sig[l,:] = utils.rrcos_resample_zeroins(frame[l,:], 1, os, beta = beta, renormalise = True)
         
-    # Verfy normalization
-    sig = utils.normalise_and_center(sig)
+        # Add AWGN
+        if snr is not None:
+            sig[l,:] = utils.add_awgn(sig[l,:],10**(-snr/20)*np.sqrt(os))
+        
+        # Add Phase Noise
+        if linewidth is not None:       
+            sig[l,:] = sig[l,:]*np.exp(1j*utils.phase_noise(sig[l,:].shape, linewidth, symb_rate * os ))
+        
+        # Add FOE
+        if freqoff is not None:
+            sig[l,:] *= np.exp(2.j * np.pi * np.arange(len(sig[l,:])) * fo / (symb_rate * os))
+        
+        # Verfy normalization
+        sig[l,:] = utils.normalise_and_center(sig[l,:])
+    
+
     
     return sig
 
@@ -97,7 +110,7 @@ os = 2
 symb_rate = 20e9
 snr = None #dB
 linewidth = None # Linewidth symbol-rate product
-fo = None # Frequency offset MHz
+fo = 1e7 # Frequency offset MHzout
 
 # Pilot Settings
 
@@ -113,7 +126,7 @@ PRBSorder=15,
 PRBSseed=None
 
 
-frame_symbs, data_symbs, pilot_symbs = gen_dataframe_withpilots(128)
+frame_symbs, data_symbs, pilot_symbs = gen_dataframe_withpilots(128,1)
 
 tx_sig = sim_tx(frame_symbs, os)
 
@@ -124,10 +137,10 @@ Plotting section to check transmitter
 
 # Plot Result
 plt.figure()
-plt.semilogy(np.fft.fftshift(np.fft.fftfreq(len(tx_sig),1/os)), np.fft.fftshift(np.abs(np.fft.fft(tx_sig))**2))
+plt.semilogy(np.fft.fftshift(np.fft.fftfreq(len(tx_sig[0,:]),1/os)), np.fft.fftshift(np.abs(np.fft.fft(tx_sig[0,:]))**2))
 
-plt.figure()
-plt.plot([symbol_seq[:100].real,symbol_seq[:100].imag],'.')
+#plt.figure()
+#plt.plot([symbol_seq[:100].real,symbol_seq[:100].imag],'.')
 
 
 
