Updated receiver and fixed error in pilot-aided CPE with averaging

dsp/pilotbased_receiver.py
@@ -79,7 +79,6 @@ def pilot_based_cpe(rec_symbs, pilot_symbs, pilot_ins_ratio, num_average = 1, us
     pilot_symbs = np.atleast_2d(pilot_symbs)
     npols = rec_symbs.shape[0]
     
-    
     # Extract the pilot symbols
     numBlocks = np.floor(np.shape(rec_symbs)[1]/pilot_ins_ratio)
     # If selected, only process a limited number of blocks. 
@@ -129,8 +128,10 @@ def pilot_based_cpe(rec_symbs, pilot_symbs, pilot_ins_ratio, num_average = 1, us
         plt.plot(pilot_phase)
         
         # Fix! Need moving average in numpy
+        print(np.shape(pilot_phase))
         pilot_phase_average = np.transpose(moving_average(pilot_phase,num_average))
-        pilot_phase = np.hstack([pilot_phase[:(num_average-1) / 2], pilot_phase_average])
+        print(np.shape(pilot_phase_average))
+        pilot_phase = np.hstack([pilot_phase[:(num_average-1)], pilot_phase_average])
            
         # Pilot positions in the received data set
         pilot_pos = np.arange(0,len(pilot_phase)*pilot_ins_ratio*use_pilot_ratio,pilot_ins_ratio*use_pilot_ratio)
@@ -371,7 +372,7 @@ for l in range(npols):
 phase_comp_symbs = []
 phase_trace = []
 for l in range(npols):   
-    symbs, trace = pilot_based_cpe(comp_test_sig[l][:,pilot_seq_len:], pilot_symbs[l,pilot_seq_len:], pilot_ins_ratio, num_average = 1)
+    symbs, trace = pilot_based_cpe(comp_test_sig[l][:,pilot_seq_len:], pilot_symbs[l,pilot_seq_len:], pilot_ins_ratio, num_average = 3)
     phase_comp_symbs.append(symbs)
     phase_trace.append(trace)
 