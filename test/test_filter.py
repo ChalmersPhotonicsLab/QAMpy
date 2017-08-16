b0de12c766fea96215e7ce752c4dfd9997f31035
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-08-16 23:16:13 +0200

    minor fix to testing of moving avg filter

--- test/test_filter.py
+++ test/test_filter.py
@@ -6,14 +6,10 @@ from dsp import filter
 
 class TestMovingAvg(object):
     def test_lengths(self):
-        l1 = []
-        l2 = []
         for i in range(100, 131):
             x = np.arange(i)
             for j in range(5, 30):
-                l1.append(len(filter.moving_average(x, j)))
-                l2.append(i-j+1)
-        assert l1 == l2
+                assert len(filter.moving_average(x, j)) == i-j+1
 
     def test_numbers1(self):
         npt.assert_allclose(np.array([6,9,12])/3, filter.moving_average(np.arange(1,6), 3))
