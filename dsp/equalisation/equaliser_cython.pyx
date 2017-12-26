727e81cdd6d8a358f8dfb2fe598896a85406cdf3
Jochen Schroeder <jochen.schroeder@gmail.com>
2017-12-26 23:56:17 +0100

    remove all uneeded code including the FS_CMA methods etc.

--- dsp/equalisation/equaliser_cython.pyx
+++ dsp/equalisation/equaliser_cython.pyx
@@ -56,22 +56,6 @@ def quantize(np.ndarray[ndim=1, dtype=np.complex128_t] E, np.ndarray[ndim=1, dty
         det_syms[i] = det_symbol(<double complex *>symbols.data, M, E[i], &dists)
     return det_syms
 
-def mrde_err(a, b):
-    cdef np.float64_t[:] partition_real = a.real
-    cdef np.float64_t[:] partition_imag = a.imag
-    cdef np.float64_t[:] codebook_real = b.real
-    cdef np.float64_t[:] codebook_imag = b.imag
-    #cdef complex double[:] codebook = a
-    def mrde_fct(double complex Xest):
-        cdef double complex Ssq
-        cdef double complex S_DD
-        #pp = np.asarray(partition)
-        #cc = np.asarray(codebook)
-        Ssq = Xest.real**2 + 1.j * Xest.imag**2
-        S_DD = partition_value(Ssq.real, partition_real, codebook_real) + 1.j * partition_value(Ssq.imag, partition_imag, codebook_imag)
-        return (Ssq.real - S_DD.real)*Xest.real + 1.j*(Ssq.imag - S_DD.imag)*Xest.imag
-    return mrde_fct
-
 cdef partition_value(double signal,
                      np.float64_t[:] partitions,
                      np.float64_t[:] codebook):
@@ -91,27 +75,6 @@ cdef double adapt_step(double mu, double complex err_p, double complex err):
     mu = mu/(1+lm*mu*(err.real*err.real + err.imag*err.imag))
     return mu
 
-def FS_CMA(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                    int TrSyms,
-                    int Ntaps,
-                    unsigned int os,
-                    double mu,
-                    np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-                    double R,
-                    bool adaptive=False):
-    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
-    cdef unsigned int i, j, k
-    cdef unsigned int pols = E.shape[0]
-    cdef unsigned int L = E.shape[1]
-    cdef np.complex128_t Xest
-    for i in range(0, TrSyms):
-        Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
-        err[i] = Xest*(Xest.real**2+Xest.imag**2 - R)
-        update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
-        if adaptive and i > 0:
-            mu = adapt_step(mu, err[i-1], err[i])
-    return err, wx
-
 cdef class ErrFct:
     cpdef double complex calc_error(self, double complex Xest)  except *:
     #cdef double complex calc_error(self, double complex Xest):
@@ -126,7 +89,6 @@ cdef class MCMAErr(ErrFct):
         self.R_real = R.real
         self.R_imag = R.imag
     cpdef double complex calc_error(self, double complex Xest)  except *:
-    #cdef double complex calc_error(self, double complex Xest):
         return (creal(Xest)**2 - self.R_real)*creal(Xest) + I*(cimag(Xest)**2 -self.R_imag)*cimag(Xest)
 
 cdef class CMAErr(ErrFct):
@@ -144,7 +106,6 @@ cdef class RDEErr(ErrFct):
         self.partition = partition
         self.codebook = codebook
     cpdef double complex calc_error(self, double complex Xest)  except *:
-    #cdef double complex calc_error(self, double complex Xest):
         cdef double Ssq
         cdef double S_DD
         Ssq = Xest.real**2 + Xest.imag**2
@@ -163,7 +124,6 @@ cdef class MRDEErr(ErrFct):
         self.codebook_real = codebook.real
         self.codebook_imag = codebook.imag
     cpdef double complex calc_error(self, double complex Xest)  except *:
-    #cdef double complex calc_error(self, double complex Xest):
         cdef double complex Ssq
         cdef double complex S_DD
         Ssq = creal(Xest)**2 + 1.j * cimag(Xest)**2
@@ -178,7 +138,6 @@ cdef class SBDErr(ErrFct):
         self.symbols = symbols
         self.N = symbols.shape[0]
     cpdef double complex calc_error(self, double complex Xest)  except *:
-    #cdef double complex calc_error(self, double complex Xest):
         cdef double complex R
         R = det_symbol(&self.symbols[0], self.N, Xest, &self.dist)
         return (Xest.real - R.real)*abs(R.real) + 1.j*(Xest.imag - R.imag)*abs(R.imag)
@@ -224,38 +183,6 @@ cdef class CMEErr(ErrFct):
     cpdef double complex calc_error(self, double complex Xest) except *:
         return (abs(Xest)**2 - self.R)*Xest + self.beta * np.pi/(2*self.d) * (sin(Xest.real*np.pi/self.d) + 1.j * sin(Xest.imag*np.pi/self.d))
 
-def mcma_err(double complex R):
-    def mcma_fct(double complex Xest):
-        return (creal(Xest)**2-creal(R))*creal(Xest)+1.j*(cimag(Xest)**2-cimag(R))*cimag(Xest)
-        #return (Xest.real**2 - R.real)*Xest.real + 1.j*(Xest.imag**2 -R.imag)*Xest.imag
-    return mcma_fct
-
-#ctypedef np.complex128_t (*cfct) (np.complex128_t)
-
-cdef double complex R
-cdef double complex mcma_fct2(double complex Xest):
-    global R
-    return (creal(Xest)**2-creal(R))*creal(Xest)+1.j*(cimag(Xest)**2-cimag(R))*cimag(Xest)
-    #return (Xest.real**2 - R.real)*Xest.real + 1.j*(Xest.imag**2 -R.imag)*Xest.imag
-
-#cdef double complex mcma_fct3(double complex Xest, tuple args):
-    #return mcma_error(Xest, args[0])
-
-def select_err(method, args):
-    if method == "mcma":
-        #global R
-        #R = args['R']
-        #return mcma_err(args['R'])
-        #return mcma_fct2
-        return MCMAErr(args['R'])
-        #return R
-    elif method == "mrde":
-        #return mrde_err(args['partition'], args['codebook'])
-        return MRDEErr(args['partition'], args['codebook'])
-    else:
-        #return mcma_err(args['R'])
-        return 0
-
 def generic_eq(np.ndarray[ndim=2, dtype=double complex] E,
                     int TrSyms,
                     int Ntaps,
@@ -263,7 +190,6 @@ def generic_eq(np.ndarray[ndim=2, dtype=double complex] E,
                     double mu,
                     np.ndarray[ndim=2, dtype=double complex] wx,
                     ErrFct errfct,
-                    #object errfct,
                     bool adaptive=False):
     cdef np.ndarray[ndim=1, dtype=double complex] err = np.zeros(TrSyms, dtype=np.complex128)
     cdef unsigned int i, j, k
@@ -274,144 +200,6 @@ def generic_eq(np.ndarray[ndim=2, dtype=double complex] E,
     for i in range(0, TrSyms):
         Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
         err[i] = errfct.calc_error(Xest)
-        #err[i] = errfct(Xest)
-        #err[i] = mcma_fct2(Xest)
-        update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
-        if adaptive and i > 0:
-            mu = adapt_step(mu, err[i-1], err[i])
-    return err, wx
-
-
-
-def FS_MCMA(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                     int TrSyms,
-                     int Ntaps,
-                     unsigned int os,
-                     double mu,
-                     np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-                     np.complex128_t R,
-                    bool adaptive=False):
-    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
-    cdef unsigned int i, j, k
-    cdef unsigned int pols = E.shape[0]
-    cdef unsigned int L = E.shape[1]
-    cdef np.complex128_t Xest
-    for i in range(0, TrSyms):
-        Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
-        err[i] = (Xest.real**2 - R.real)*Xest.real + 1.j*(Xest.imag**2 - R.imag)*Xest.imag
-        update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
-        if adaptive and i > 0:
-            mu = adapt_step(mu, err[i-1], err[i])
-    return err, wx
-
-def FS_RDE(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                    int TrSyms,
-                    int Ntaps,
-                    unsigned int os,
-                    double mu,
-                    np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-                    np.ndarray[ndim=1, dtype=np.float64_t] partition,
-                    np.ndarray[ndim=1, dtype=np.float64_t] codebook,
-                    bool adaptive=False):
-    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
-    cdef unsigned int i, j, k
-    cdef unsigned int pols = E.shape[0]
-    cdef unsigned int L = E.shape[1]
-    cdef double complex Xest
-    cdef double Ssq, S_DD
-    for i in range(TrSyms):
-        Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
-        Ssq = Xest.real**2 + Xest.imag**2
-        S_DD = partition_value(Ssq, partition, codebook)
-        err[i] = Xest*(Ssq - S_DD)
-        update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
-        if adaptive and i > 0:
-            mu = adapt_step(mu, err[i-1], err[i])
-    return err, wx
-
-def FS_MRDE(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                     int TrSyms, int Ntaps, unsigned int os,
-                     double mu,
-                     np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-                     np.ndarray[ndim=1, dtype=np.complex128_t] partition,
-                     np.ndarray[ndim=1, dtype=np.complex128_t] codebook,
-                    bool adaptive=False):
-    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
-    cdef unsigned int i, j, k
-    cdef unsigned int pols = E.shape[0]
-    cdef unsigned int L = E.shape[1]
-    cdef np.complex128_t Xest, Ssq, S_DD
-    for i in range(TrSyms):
-        Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
-        Ssq = Xest.real**2 + 1.j * Xest.imag**2
-        S_DD = partition_value(Ssq.real, partition.real, codebook.real) + 1.j * partition_value(Ssq.imag, partition.imag, codebook.imag)
-        err[i] = (Ssq.real - S_DD.real)*Xest.real + 1.j*(Ssq.imag - S_DD.imag)*Xest.imag
-        update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
-        if adaptive and i > 0:
-            mu = adapt_step(mu, err[i-1], err[i])
-    return err, wx
-
-def FS_SBD(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                     int TrSyms, int Ntaps, unsigned int os,
-                     double mu,
-                     np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-                     np.ndarray[ndim=1, dtype=np.complex128_t] symbols,
-                     bool adaptive=False):
-    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
-    cdef unsigned int i, j, k, N
-    cdef double complex Xest, R
-    cdef unsigned int pols = E.shape[0]
-    cdef unsigned int M = len(symbols)
-    cdef double lm, dist
-    cdef unsigned int L = E.shape[1]
-    for i in range(TrSyms):
-        Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
-        R = det_symbol(&symbols[0], M, Xest, &dist)
-        err[i] = (Xest.real - R.real)*abs(R.real) + 1.j*(Xest.imag - R.imag)*abs(R.imag)
-        update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
-        if adaptive and i > 0:
-            mu = adapt_step(mu, err[i-1], err[i])
-    return err, wx
-
-def FS_MDDMA(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                     int TrSyms, int Ntaps, unsigned int os,
-                     double mu,
-                     np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-                     np.ndarray[ndim=1, dtype=np.complex128_t] symbols,
-                     bool adaptive=False):
-    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
-    cdef unsigned int i, j, k
-    cdef np.complex128_t Xest, R
-    cdef unsigned int M = len(symbols)
-    cdef unsigned int pols = E.shape[0]
-    cdef unsigned int L = E.shape[1]
-    cdef double dist
-    for i in range(TrSyms):
-        Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
-        R = det_symbol(&symbols[0], M, Xest, &dist)
-        err[i] = (Xest.real**2 - R.real**2)*Xest.real + 1.j*(Xest.imag**2 - R.imag**2)*Xest.imag
-        update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
-        if adaptive and i > 0:
-            mu = adapt_step(mu, err[i-1], err[i])
-    return err, wx
-
-def FS_DD(np.ndarray[ndim=2, dtype=np.complex128_t] E,
-                     int TrSyms, int Ntaps, unsigned int os,
-                     double mu,
-                     np.ndarray[ndim=2, dtype=np.complex128_t] wx,
-                     np.ndarray[ndim=1, dtype=np.complex128_t] symbols,
-                     bool adaptive=False):
-    cdef np.ndarray[ndim=1, dtype=np.complex128_t] err = np.zeros(TrSyms, dtype=np.complex128)
-    cdef unsigned int i, j, k
-    cdef np.complex128_t Xest, R
-    cdef unsigned int M = len(symbols)
-    cdef unsigned int pols = E.shape[0]
-    cdef unsigned int L = E.shape[1]
-    cdef double dist
-    for i in range(TrSyms):
-        Xest = apply_filter(&E[0, i*os], Ntaps, &wx[0,0], pols, L)
-        R = det_symbol(&symbols[0], M, Xest, &dist)
-        err[i] = (Xest - R)
         update_filter(&E[0,i*os], Ntaps, mu, err[i], &wx[0,0], pols, L)
         if adaptive and i > 0:
             mu = adapt_step(mu, err[i-1], err[i])
