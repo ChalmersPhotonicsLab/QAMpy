26b7a5fd7503b3434b84ea98b772b5c950b420bf
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-01-10 11:34:25 +0100

    rename some functions

--- test/test_ber_functions.py
+++ test/test_ber_functions.py
@@ -12,7 +12,7 @@ class TestFindSequenceOffset(object):
     @pytest.mark.parametrize("shiftN", [np.random.randint(l*3*10**4//4+1, (l+1)*3*10**4//4) for l in range(4)])
     @pytest.mark.parametrize("snr", [5, 20, 40])
     @pytest.mark.parametrize("N1", [None, 4000])
-    def test_find_sequence_offset_shift(self, shiftN, snr, N1):
+    def test_shift(self, shiftN, snr, N1):
         N = 3*10**4 # needs to be larger than PRBS pattern
         sig, syms, bits = self.Q.generate_signal(N, snr, dual_pol=False)
         sig2 = np.roll(sig, shift=shiftN)
@@ -21,7 +21,7 @@ class TestFindSequenceOffset(object):
 
     @pytest.mark.parametrize("shiftN", [np.random.randint(l*(2**15-1)//2+1, (l+1)*(2**15-1)//2) for l in range(4)]+[48630])
     @pytest.mark.parametrize("N1", [None, 4000])
-    def test_find_sequence_offset_data(self, shiftN, N1):
+    def test_data(self, shiftN, N1):
         N = 2*(2**15-1)
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01)
         sig2 = np.roll(sig, shift=shiftN)
@@ -38,7 +38,7 @@ class TestFindSequenceOffsetComplex(object):
     @pytest.mark.parametrize("i", range(4))
     @pytest.mark.parametrize("snr", [5, 20, 40])
     @pytest.mark.parametrize("N1", [None, 4000])
-    def test_find_sequence_offset_complex_shift(self, shiftN, i, snr, N1):
+    def test_shift(self, shiftN, i, snr, N1):
         N = 2*(2**15-1)
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01, PRBS=False)
         sig2 = np.roll(sig, shift=shiftN)
@@ -48,7 +48,7 @@ class TestFindSequenceOffsetComplex(object):
     @pytest.mark.parametrize("shiftN", [np.random.randint(l*(2**15-1)//2+1, (l+1)*(2**15-1)//2) for l in range(4)]+[48630])
     @pytest.mark.parametrize("i", range(4))
     @pytest.mark.parametrize("N1", [None, 4000])
-    def test_find_sequence_offset_complex_data(self, shiftN, i, N1):
+    def test_data(self, shiftN, i, N1):
         N = 2*(2**15-1)
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01)
         sig2 = np.roll(sig, shift=shiftN)
@@ -58,7 +58,7 @@ class TestFindSequenceOffsetComplex(object):
 
     @pytest.mark.parametrize("shiftN", [np.random.randint(l*(2**15-1)//2+1, (l+1)*(2**15-1)//2) for l in range(4)]+[48630])
     @pytest.mark.parametrize("i", range(4))
-    def test_find_sequence_offset_complex_test_rotation(self, shiftN, i):
+    def test_rotation(self, shiftN, i):
         N = 2*(2**15-1)
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01)
         sig2 = np.roll(sig, shift=shiftN)
@@ -70,7 +70,7 @@ class TestSyncAndAdjust(object):
     d = np.diff(np.unique(Q.symbols.real)).min()
 
     @pytest.mark.parametrize("N1, N2, adjust", [(None, 1000, "tx"),(None, 1000, "rx" ), (1000, None, "tx"), (1000, None, "rx") ])
-    def test_sync_adjust_test_length(self, N1, N2, adjust):
+    def test_length(self, N1, N2, adjust):
         N = 3*10**4 # needs to be larger than PRBS pattern
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01, PRBS=False)
         tx, rx = ber_functions.sync_and_adjust(sig[:N1], sig[:N2], adjust=adjust)
@@ -83,7 +83,7 @@ class TestSyncAndAdjust(object):
 
     @pytest.mark.parametrize("shiftN", [np.random.randint(l*(2**15-1)//2+1, (l+1)*(2**15-1)//2) for l in range(4)]+[48630])
     @pytest.mark.parametrize("adjust", ["tx", "rx"])
-    def test_sync_adjust_offset_same_length(self, adjust, shiftN):
+    def test_flip(self, adjust, shiftN):
         Np = 2**15-1
         N = 2*Np
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01, PRBS=True)
@@ -98,7 +98,7 @@ class TestSyncAndAdjust(object):
     @pytest.mark.parametrize("tx_i, rx_i", list(zip(list(range(1,4)) + 3*[0], 3*[0]+list(range(1,4)))))
     @pytest.mark.parametrize("N1, N2", [(None, 2**15-1), (2**15-1, None)])
     @pytest.mark.parametrize("shiftN", [np.random.randint(l*(2**15-1)//2+1, (l+1)*(2**15-1)//2) for l in range(4)] + [48630])
-    def test_sync_adjust_offset_rotated(self, adjust, tx_i, rx_i, N1, N2, shiftN):
+    def test_rotated_and_diff_length(self, adjust, tx_i, rx_i, N1, N2, shiftN):
         Np = 2**15-1
         N = 2*Np
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01, PRBS=True)
@@ -112,7 +112,7 @@ class TestAdjustDataLength(object):
 
     @pytest.mark.parametrize("N1, N2", [(None, 1000), (1000, None)])
     @pytest.mark.parametrize("method", ["truncate", "extend", None])
-    def test_adjust_data_length(self, N1, N2, method):
+    def test_length(self, N1, N2, method):
         N = 3*10**4 # needs to be larger than PRBS pattern
         sig, syms, bits = self.Q.generate_signal(N, None, dual_pol=False, beta=0.01, PRBS=False)
         tx, rx = ber_functions.adjust_data_length(sig[:N1], sig[:N2], method=method)
