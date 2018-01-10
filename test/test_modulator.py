bd79d51a2c6b50354ac2b8167d7a310616f9ed3d
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-01-10 12:12:28 +0100

    add test cases for ber and ser

--- test/test_modulator.py
+++ test/test_modulator.py
@@ -1,11 +1,74 @@
+import pytest
 import numpy as np
+import random
 import numpy.testing as npt
 import matplotlib.pylab as plt
 
 from dsp import modulation
 
+def _flip_symbols(sig, idx, d):
+    for i in idx:
+        if np.random.randint(0,2):
+            if sig[i].real > 0:
+                sig[i] -= d
+            else:
+                sig[i] += d
+        else:
+            if sig[i].imag > 0:
+                sig[i] -= 1.j*d
+            else:
+                sig[i] += 1.j*d
+    return sig
 
 class TestModulatorAttr(object):
     Q = modulation.QAMModulator(16)
-    def test_SER(self):
+    d = np.diff(np.unique(Q.symbols.real)).min()
+
+    def test_quantize_symbols_set(self):
+        """Check if all quantized symbols are from Q.symbols"""
+        sig, sym, bits = self.Q.generate_signal(2**10, 30, beta=0.01, dual_pol=False)
+        sym_demod = self.Q.quantize(sig)
+        out = np.in1d(sym_demod, self.Q.symbols)
+        assert np.alltrue(out)
+
+    def test_quantize_symbols_correct(self):
+        """Check if all quantized symbols are from Q.symbols"""
+        sig, sym, bits = self.Q.generate_signal(2**10, None, beta=0.01, dual_pol=False)
+        sym_demod = self.Q.quantize(sig)
+        npt.assert_allclose(sym_demod, sym)
+
+    def test_decode_correct(self):
+        sig, sym, bits = self.Q.generate_signal(2**10, None, beta=0.01, dual_pol=False)
+        bb = self.Q.decode(sym)
+        npt.assert_array_equal(bb, bits)
+
+    def test_gen_signal(self):
         pass
+
+    @pytest.mark.parametrize("Nerrors", range(5))
+    @pytest.mark.parametrize("shiftN", np.random.randint(0,2**10, size=10))
+    def test_ser(self, Nerrors, shiftN):
+        sig, sym, bits = self.Q.generate_signal(2**10, None, beta=0.01, dual_pol=False)
+        idx = random.sample(range(sig.shape[0]), Nerrors)
+        _flip_symbols(sig, idx, self.d)
+        sig = np.roll(sig, shift=shiftN)
+        ser = self.Q.cal_ser(sig, symbols_tx=sym)
+        npt.assert_almost_equal(ser, Nerrors/sig.shape[0])
+
+    @pytest.mark.parametrize("Nerrors", range(5))
+    @pytest.mark.parametrize("shiftN", np.random.randint(0,2**10, size=10))
+    def test_ber(self, Nerrors, shiftN):
+        sig, sym, bits = self.Q.generate_signal(2**10, None, beta=0.01, dual_pol=False)
+        idx = random.sample(range(sig.shape[0]), Nerrors)
+        _flip_symbols(sig, idx, self.d)
+        sig = np.roll(sig, shift=shiftN)
+        ber = self.Q.cal_ber(sig, bits_tx=bits)
+        npt.assert_almost_equal(ber, Nerrors/(sig.shape[0]*self.Q.bits))
+
+
+
+
+
+
+
+
