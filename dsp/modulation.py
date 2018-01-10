fix ber calculation and test for M-D

dsp/modulation.py
@@ -101,9 +101,17 @@ class QAMModulator(object):
         outbits   : array_like
             1D array of booleans representing bits
         """
-        bt = bitarray()
-        bt.encode(self._encoding, symbols.view(dtype="S16"))
-        return np.fromstring(bt.unpack(zero=b'\x00', one=b'\x01'), dtype=np.bool)
+        if symbols.ndim is 1:
+            bt = bitarray()
+            bt.encode(self._encoding, symbols.view(dtype="S16"))
+            return np.fromstring(bt.unpack(zero=b'\x00', one=b'\x01'), dtype=np.bool)
+        bits = []
+        for i in range(symbols.shape[0]):
+            bt = bitarray()
+            bt.encode(self._encoding, symbols[i].view(dtype="S16"))
+            bits.append(np.fromstring(bt.unpack(zero=b'\x00', one=b'\x01'), dtype=np.bool) )
+        return np.array(bits)
+
 
     def quantize(self, signal):
         """
@@ -269,17 +277,23 @@ class QAMModulator(object):
         N            : integer
             length of input sequence
         """
-        assert bits_tx is not None or PRBS is not None, "either bits_tx or PRBS needs to be given"
+        signal_rx = np.atleast_2d(signal_rx)
+        ndim = signal_rx.shape[0]
         syms_demod = self.quantize(signal_rx)
         if symbols_tx is None:
             if bits_tx is None:
-                bits_tx = make_prbs_extXOR( PRBS[0], len(syms_demod)*self.Nbits, seed=PRBS[1])
-            symbols_tx = self.modulate(bits_tx)
+                symbols_tx = self.symbols_tx
+            else:
+                symbols_tx = []
+                for i in range(ndim):
+                    symbols_tx.append(self.modulate(bits_tx[i]))
+                symbols_tx = np.array(symbols_tx)
         if not synced:
-            s_tx_sync, syms_demod = ber_functions.sync_and_adjust(symbols_tx, syms_demod)
+            symbols_tx, syms_demod = self._sync_and_adjust(symbols_tx, syms_demod)
         bits_demod = self.decode(syms_demod)
-        tx_synced = self.decode(s_tx_sync)
-        return ber_functions.cal_ber_syncd(tx_synced, bits_demod, threshold=0.8)[0]
+        tx_synced = self.decode(symbols_tx)
+        errs = np.count_nonzero(tx_synced - bits_demod)
+        return errs/(bits_demod.shape[1]*ndim)
 
     def cal_evm(self, signal_rx, symbols_tx=None):
         """

test/test_modulator.py
@@ -59,15 +59,15 @@ class TestModulatorAttr(object):
 
     @pytest.mark.parametrize("Nerrors", range(5))
     @pytest.mark.parametrize("shiftN", np.random.randint(0,2**10, size=10))
-    def test_ber(self, Nerrors, shiftN):
-        sig, sym, bits = self.Q.generate_signal(2 ** 10, None, beta=0.01, ndim=1)
-        sig = sig[0]
-        bits = bits[0]
-        idx = random.sample(range(sig.shape[0]), Nerrors)
-        _flip_symbols(sig, idx, self.d)
-        sig = np.roll(sig, shift=shiftN)
-        ber = self.Q.cal_ber(sig, bits_tx=bits)
-        npt.assert_almost_equal(ber, Nerrors/(sig.shape[0]*self.Q.Nbits))
+    @pytest.mark.parametrize("ndims", range(1,3))
+    def test_ber(self, Nerrors, shiftN, ndims):
+        sig, sym, bits = self.Q.generate_signal(2 ** 10, None, beta=0.01, ndim=ndims)
+        for i in range(ndims):
+            idx = random.sample(range(sig.shape[1]), Nerrors)
+            _flip_symbols(sig[i], idx, self.d)
+        sig = np.roll(sig, shift=shiftN, axis=-1)
+        ber = self.Q.cal_ber(sig)
+        npt.assert_almost_equal(ber, Nerrors/(sig.shape[1]*self.Q.Nbits))
 
 
 