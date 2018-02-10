6cca324e8edc62bdcb27e27aa3abe6838a6a3e7b
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-10 23:57:36 +0100

    add tests to check subclass preserve for filters

--- test/test_filter.py
+++ test/test_filter.py
@@ -1,7 +1,8 @@
+import pytest
 import numpy as np
 import numpy.testing as npt
 
-from dsp import filter
+from dsp import filter, modulation
 
 
 class TestMovingAvg(object):
@@ -21,3 +22,50 @@ class TestMovingAvg(object):
         npt.assert_allclose(np.array([6,9,12,15])/3, filter.moving_average(np.arange(1,7), 3))
 
 
+class TestReturnObjects(object):
+    s = modulation.ResampledQAM(16, 2**14, fs=2, nmodes=2)
+
+    def test_pre_filter(self):
+        s2 = filter.pre_filter(self.s, 0.01)
+        assert type(self.s) is type(s2)
+
+    def test_filter_signal(self):
+        s2 = filter.filter_signal(self.s, self.s.fs, 0.001)
+        assert type(self.s) is type(s2)
+
+    def test_filter_signal_analog(self):
+        s2 = filter.filter_signal_analog(self.s, self.s.fs, 0.001)
+        assert type(self.s) is type(s2)
+
+    def test_rrcos_pulseshaping(self):
+        s2 = filter.rrcos_pulseshaping(self.s, self.s.fs, 1/self.s.fb, 0.1)
+        assert type(self.s) is type(s2)
+
+    def test_mvg_avg(self):
+        s2 = filter.moving_average(self.s)
+        assert type(self.s) is type(s2)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_pre_filter_attr(self, attr):
+        s2 = filter.pre_filter(self.s, 0.01)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_filter_signal_attr(self, attr):
+        s2 = filter.filter_signal(self.s, self.s.fs, 0.01)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_filter_signal_analog_attr(self, attr):
+        s2 = filter.filter_signal_analog(self.s, self.s.fs, 0.01)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_rrcos_pulseshaping_attr(self, attr):
+        s2 = filter.rrcos_pulseshaping(self.s, self.s.fs, 1/self.s.fb, 0.1)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_mvg_avg_attr(self, attr):
+        s2 = filter.moving_average(self.s)
+        assert getattr(self.s, attr) is getattr(s2, attr)
