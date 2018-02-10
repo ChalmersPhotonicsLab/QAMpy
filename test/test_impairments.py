e36f2697c06a050ffd4592a67a544431b6694670
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-10 23:32:22 +0100

    test that attr remain

--- test/test_impairments.py
+++ test/test_impairments.py
@@ -22,6 +22,27 @@ class TestReturnObjects(object):
         s2 = impairments.apply_phase_noise(self.s, 1e-3, self.s.fs)
         assert type(self.s) is type(s2)
 
-    def test_add_awgn(self):
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_add_awgn(self, attr):
         s2 = impairments.add_awgn(self.s, 0.01)
-        assert type(self.s) is type(s2)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_rotate_field_attr(self, attr):
+        s2 = impairments.rotate_field(self.s, np.pi/3)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_apply_PMD_attr(self, attr):
+        s2 = impairments.apply_PMD_to_field(self.s, np.pi/3, 1e-3, self.s.fs)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_apply_phase_noise_attr(self, attr):
+        s2 = impairments.apply_phase_noise(self.s, 1e-3, self.s.fs)
+        assert getattr(self.s, attr) is getattr(s2, attr)
+
+    @pytest.mark.parametrize("attr", ["fs", "symbols", "fb"])
+    def test_add_awgn_attr(self, attr):
+        s2 = impairments.add_awgn(self.s, 0.01)
+        assert getattr(self.s, attr) is getattr(s2, attr)
