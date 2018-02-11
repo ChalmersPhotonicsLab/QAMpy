1785a1908aaf885dcbf2e3870c5d0e7c5865b255
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-11 21:04:21 +0100

    tests for equaliser functions to work with SignalObjects

---
+++ test/test_equalisation.py
@@ -0,0 +1,17 @@
+import pytest
+import numpy as np
+import numpy.testing as npt
+
+from dsp import modulation, impairments, equalisation
+
+class TestReturnObject(object):
+    s = modulation.ResampledQAM(16, 2**16, fb=20e9, fs=40e9, nmodes=2)
+    os = 2
+
+    def test_apply_filter(self):
+        s2 = impairments.simulate_transmission(self.s, self.s.fb, self.s.fs, snr=20, dgd=100e-12)
+        wx, err = equalisation.equalise_signal(s2, self.os, 1e-3, s2.M, Ntaps=11)
+        s3 = equalisation.apply_filter(s2, self.os, wx)
+        assert type(s3) is type(self.s)
+
+
