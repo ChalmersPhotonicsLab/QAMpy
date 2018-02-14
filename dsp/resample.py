dd45973336fe3e4265790fc1ec08a9b6c4a99343
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-14 22:54:40 +0100

    resample rrcos now fixed

--- dsp/resample.py
+++ dsp/resample.py
@@ -47,7 +47,7 @@ def resample_poly(signal, fold, fnew, window=None, renormalise=False):
         sig_new = normalise_and_center(sig_new)*np.sqrt(p)
     return sig_new
 
-def rrcos_resample(signal, fold, fnew, Ts=None, beta=None, taps=4001, renormalise=False):
+def rrcos_resample(signal, fold, fnew, Ts=None, beta=None, taps=4001, renormalise=False, fftconv=False):
     """
     Resample a signal using a root raised cosine filter. This performs pulse shaping and resampling a the same time.
     The resampling is done by scipy.signal.resample_poly. This function can be quite slow.
@@ -64,8 +64,12 @@ def rrcos_resample(signal, fold, fnew, Ts=None, beta=None, taps=4001, renormalis
         time width of the RRCOS filter (default:None makes this 1/fold)
     beta     : float, optional
         filter roll off factor between (0,1] (default:None will use the default filter in poly_resample)
-    taps : float, optional
-        taps of the interpolation filter
+    taps : int, optional
+        taps of the interpolation filter if taps is None we filter by zeroinsertion upsampling and multipling
+        with the full length rrcos frequency response in the spectral domain
+    fftconv : bool, optional
+        filter using zero insertion upsampling/decimation and filtering using fftconvolve. I often faster for
+        long filters and power of two signal lengths.
 
     Returns
     -------
@@ -79,13 +83,15 @@ def rrcos_resample(signal, fold, fnew, Ts=None, beta=None, taps=4001, renormalis
         Ts = 1/fold
     up, down = _resamplingfactors(fold, fnew)
     fup = up*fold
-    if taps is None or taps > signal.size/4 or taps > 2**13:
+    # for very large tap lengths it is better to filter with fftconvolve
+    # it could be better to use default values here, but this seems to depend on pc
+    if fftconv or taps is None:
         sig_new = np.zeros((up*signal.size,), dtype=signal.dtype)
         sig_new[::up] = signal
-        sig_new = rrcos_pulseshaping(sig_new, fnew, Ts, beta, taps)
+        sig_new = rrcos_pulseshaping(sig_new, fup, Ts, beta, taps)
         sig_new = sig_new[::down]
     else:
-        t = np.linspace(0, taps, endpoint=False)
+        t = np.linspace(0, taps, taps, endpoint=False)
         t -= t[(t.size-1)//2]
         t /= fup
         nqf = rrcos_time(t, beta, Ts)
