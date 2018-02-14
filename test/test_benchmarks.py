e2e5d3bee869a28b5024f7a45552d27bfc374ac5
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-14 23:27:23 +0100

    add benchmarking tests of resample code

---
+++ test/test_benchmarks.py
@@ -0,0 +1,13 @@
+import pytest
+import numpy as np
+
+from dsp import resample
+
+
+@pytest.mark.parametrize("arr", [np.random.randn(N)+1.j*np.random.randn(N)
+                                 for N in [2**16, 10**6+1 ]])
+@pytest.mark.parametrize("taps", [100, 1000, 4000])
+@pytest.mark.parametrize("fconv", [True, False])
+def test_resampling_benchmark(arr, taps, fconv, benchmark):
+    benchmark.group = "resample N-%d taps-%d"%(arr.size, taps)
+    s = benchmark(resample.rrcos_resample, arr, 1, 3, 1, beta=0.1, taps=taps, fftconv=fconv )
