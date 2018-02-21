72f8178e006df073cfa6ea9f56a2a7f0ca45f782
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-21 23:28:11 +0100

    start to implement some LMS DSP, still needs fixes

--- test/test_signal_recover_functional.py
+++ test/test_signal_recover_functional.py
@@ -5,6 +5,8 @@ import numpy.testing as npt
 
 from dsp import modulation, helpers, phaserec, equalisation, impairments
 
+import matplotlib.pylab as plt
+
 @pytest.mark.parametrize("lw", np.linspace(10, 1000, 4))
 @pytest.mark.parametrize("M", [4, 16, 32, 64])
 def test_phaserec_bps(lw, M):
@@ -43,79 +45,89 @@ def test_phaserec_bps_2stage(lw, M):
     ser = recoverd.cal_ser()
     npt.assert_allclose(ser, 0)
 
-class TestCMA(object):
-    @pytest.mark.parametrize("method", ["cma", "mcma"])
+class TestDualMode(object):
+    @pytest.mark.parametrize("method1", ["cma", "mcma"])
+    @pytest.mark.parametrize("method2", ["sbd", "mddma"])
     @pytest.mark.parametrize("phi", np.linspace(4.3, 8, 5))
-    def test_pol_rot(self, method, phi):
+    def test_pol_rot(self, method1, method2, phi):
         phi = np.pi/phi
         fb = 40.e9
         os = 2
         fs = os*fb
         N = 2**16
-        beta = 0.1
-        mu = 0.1e-1
-        M = 4
+        beta = 0.9
+        mu1 = 0.1e-2
+        mu2 = 0.1e-2
+        M = 32
         s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
-        s = s.resample(fs, beta=0.1, renormalise=True)
+        s = s.resample(fs, beta=beta, renormalise=True)
         s = impairments.rotate_field(s, phi)
-        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=3, method=method, adaptive_stepsize=True)
-        sout = equalisation.apply_filter(s, wxy)
+        sout, wxy, err = equalisation.dual_mode_equalisation(s, (mu1,mu2), M, Ntaps=3, methods=(method1, method2), adaptive_stepsize=(True,True))
+        sout = helpers.normalise_and_center(sout)
         ser = sout.cal_ser()
+        #plt.plot(sout[0].real, sout[0].imag, 'r.')
+        #plt.plot(sout[1].real, sout[1].imag, 'b.')
+        #plt.show()
+        if ser.mean() > 0.5:
+            ser = sout[::-1].cal_ser()
         npt.assert_allclose(ser, 0)
 
-    @pytest.mark.parametrize("method", ["cma", "mcma"])
-    @pytest.mark.parametrize("phi", np.linspace(4.3, 6.5, 3))
-    @pytest.mark.parametrize("dgd", np.linspace(10, 300, 4)*1e-12)
-    def test_pmd(self, method, phi, dgd):
+    @pytest.mark.parametrize("method1", ["cma", "mcma"])
+    @pytest.mark.parametrize("method2", ["sbd", "mddma"])
+    @pytest.mark.parametrize("phi", np.linspace(4.3, 4.8, 2))
+    @pytest.mark.parametrize("dgd", np.linspace(10, 70, 2)*1e-12)
+    def test_pmd(self, method1, method2, phi, dgd):
         theta = np.pi/phi
         fb = 40.e9
         os = 2
         fs = os*fb
         N = 2**16
         snr = 15
-        beta = 0.1
-        mu = 4e-5
-        M = 4
+        beta = 0.99
+        mu1 = 4e-2
+        mu2 = 4e-2
+        M = 32
         ntaps = 21
         s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
-        s = s.resample(fs, beta=0.1, renormalise=True)
+        s = s.resample(fs, beta=beta, renormalise=True)
         s = impairments.apply_PMD_to_field(s, theta, dgd)
-        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, method=method, adaptive_stepsize=False)
-        sout = equalisation.apply_filter(s, wxy)
+        sout, wxy, err = equalisation.dual_mode_equalisation(s, (mu1,mu2), M, Ntaps=3, methods=(method1, method2), adaptive_stepsize=(True,True))
         sout = helpers.normalise_and_center(sout)
         ser = sout.cal_ser()
-        npt.assert_allclose(ser, 0)
+        npt.assert_allclose(ser, 0, atol=1.01*2/N) # can tolerate 1-2 errors
 
-    #TODO: figure out the failures for small angles
-    @pytest.mark.xfail
-    @pytest.mark.parametrize("method", ["cma", "mcma"])
-    @pytest.mark.parametrize("phi", np.linspace(7.5, 8.5, 2))
-    @pytest.mark.parametrize("dgd", np.linspace(200, 300, 2)*1e-12)
-    def test_pmd_fails(self, method, phi, dgd):
-        theta = np.pi/phi
-        fb = 40.e9
-        os = 2
-        fs = os*fb
-        N = 2**16
-        snr = 15
-        beta = 0.1
-        mu = 1e-4
-        M = 4
-        ntaps = 21
-        s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
-        s = s.resample(fs, beta=0.1, renormalise=True)
-        s = impairments.apply_PMD_to_field(s, theta, dgd)
-        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, Niter=3, method=method, adaptive_stepsize=False)
-        sout = equalisation.apply_filter(s, wxy)
-        sout = helpers.normalise_and_center(sout)
-        ser = sout.cal_ser()
-        npt.assert_allclose(ser, 0, atol=1.01*4/N)
+    ##TODO: figure out the failures for small angles
+    #@pytest.mark.xfail
+    #@pytest.mark.parametrize("method1", ["cma", "mcma"])
+    #@pytest.mark.parametrize("method2", ["sbd", "mddma"])
+    #@pytest.mark.parametrize("phi", np.linspace(7.5, 8.5, 2))
+    #@pytest.mark.parametrize("dgd", np.linspace(200, 300, 2)*1e-12)
+    #def test_pmd_fails(self, method1, method2, phi, dgd):
+    #    theta = np.pi/phi
+    #    fb = 40.e9
+    #    os = 2
+    #    fs = os*fb
+    #    N = 2**16
+    #    snr = 15
+    #    beta = 0.9
+    #    mu1 = 4e-3
+    #    mu2 = 4e-3
+    #    M = 32
+    #    ntaps = 21
+    #    s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
+    #    s = s.resample(fs, beta=0.1, renormalise=True)
+    #    s = impairments.apply_PMD_to_field(s, theta, dgd)
+    #    sout, wxy, err = equalisation.dual_mode_equalisation(s, (mu1,mu2), M, Ntaps=3, methods=(method1, method2), adaptive_stepsize=(True,True))
+    #    sout = helpers.normalise_and_center(sout)
+    #    ser = sout.cal_ser()
+    #    npt.assert_allclose(ser, 0, atol=1.01*4/N)
 
-    @pytest.mark.parametrize("method", ["cma", "mcma"])
+    @pytest.mark.parametrize("method1", ["cma", "mcma"])
+    @pytest.mark.parametrize("method2", ["sbd", "mddma"])
     @pytest.mark.parametrize("phi", np.linspace(4.3, 5.7, 2))
     @pytest.mark.parametrize("dgd", np.linspace(10, 300, 3)*1e-12)
     @pytest.mark.parametrize("lw", np.linspace(10e3, 1000e3, 3))
-    def test_pmd_phase(self, method, phi, dgd, lw):
+    def test_pmd_phase(self, method1, method2, phi, dgd, lw):
         theta = np.pi/phi
         fb = 40.e9
         os = 2
@@ -130,7 +142,7 @@ class TestCMA(object):
         s = s.resample(fs, beta=0.1, renormalise=True)
         s = impairments.apply_phase_noise(s, lw)
         s = impairments.apply_PMD_to_field(s, theta, dgd)
-        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, method=method, adaptive_stepsize=False)
+        sout, wxy, err = equalisation.dual_mode_equalisation(s, (mu1,mu2), M, Ntaps=3, methods=(method1, method2), adaptive_stepsize=(True,True))
         sout = equalisation.apply_filter(s, wxy)
         sout, ph = phaserec.viterbiviterbi(sout,11)
         sout = helpers.normalise_and_center(sout)
@@ -156,6 +168,130 @@ class TestCMA(object):
         ntaps = 21
         s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
         s = s.resample(fs, beta=0.1, renormalise=True)
+        s = impairments.apply_phase_noise(s, lw)
+        s = impairments.apply_PMD_to_field(s, theta, dgd)
+        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, method=method, adaptive_stepsize=False)
+        sout = equalisation.apply_filter(s, wxy)
+        sout, ph = phaserec.viterbiviterbi(sout,11)
+        sout = helpers.normalise_and_center(sout)
+        sout = helpers.dump_edges(sout, 20)
+        ser = sout.cal_ser()
+        npt.assert_allclose(ser, 0)
+
+
+class TestCMA(object):
+    @pytest.mark.parametrize("method", ["cma", "mcma"])
+    @pytest.mark.parametrize("phi", np.linspace(4.3, 8, 5))
+    def test_pol_rot(self, method, phi):
+        phi = np.pi/phi
+        fb = 40.e9
+        os = 2
+        fs = os*fb
+        N = 2**16
+        beta = 0.1
+        mu = 0.1e-1
+        M = 4
+        s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
+        s = s.resample(fs, beta=beta, renormalise=True)
+        s = impairments.rotate_field(s, phi)
+        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=3, method=method, adaptive_stepsize=True)
+        sout = equalisation.apply_filter(s, wxy)
+        ser = sout.cal_ser()
+        npt.assert_allclose(ser, 0)
+
+    @pytest.mark.parametrize("method", ["cma", "mcma"])
+    @pytest.mark.parametrize("phi", np.linspace(4.3, 6.5, 3))
+    @pytest.mark.parametrize("dgd", np.linspace(10, 300, 4)*1e-12)
+    def test_pmd(self, method, phi, dgd):
+        theta = np.pi/phi
+        fb = 40.e9
+        os = 2
+        fs = os*fb
+        N = 2**16
+        snr = 15
+        beta = 0.1
+        mu = 4e-5
+        M = 4
+        ntaps = 21
+        s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
+        s = s.resample(fs, beta=beta, renormalise=True)
+        s = impairments.apply_PMD_to_field(s, theta, dgd)
+        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, method=method, adaptive_stepsize=False)
+        sout = equalisation.apply_filter(s, wxy)
+        sout = helpers.normalise_and_center(sout)
+        ser = sout.cal_ser()
+        npt.assert_allclose(ser, 0)
+
+    #TODO: figure out the failures for small angles
+    @pytest.mark.xfail
+    @pytest.mark.parametrize("method", ["cma", "mcma"])
+    @pytest.mark.parametrize("phi", np.linspace(7.5, 8.5, 2))
+    @pytest.mark.parametrize("dgd", np.linspace(200, 300, 2)*1e-12)
+    def test_pmd_fails(self, method, phi, dgd):
+        theta = np.pi/phi
+        fb = 40.e9
+        os = 2
+        fs = os*fb
+        N = 2**16
+        snr = 15
+        beta = 0.6
+        mu = 1e-4
+        M = 4
+        ntaps = 21
+        s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
+        s = s.resample(fs, beta=beta, renormalise=True)
+        s = impairments.apply_PMD_to_field(s, theta, dgd)
+        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, Niter=3, method=method, adaptive_stepsize=False)
+        sout = equalisation.apply_filter(s, wxy)
+        sout = helpers.normalise_and_center(sout)
+        ser = sout.cal_ser()
+        npt.assert_allclose(ser, 0, atol=1.01*4/N)
+
+    @pytest.mark.parametrize("method", ["cma", "mcma"])
+    @pytest.mark.parametrize("phi", np.linspace(4.3, 5.7, 2))
+    @pytest.mark.parametrize("dgd", np.linspace(10, 300, 3)*1e-12)
+    @pytest.mark.parametrize("lw", np.linspace(10e3, 1000e3, 3))
+    def test_pmd_phase(self, method, phi, dgd, lw):
+        theta = np.pi/phi
+        fb = 40.e9
+        os = 2
+        fs = os*fb
+        N = 2**16
+        snr = 15
+        beta = 0.3
+        mu = 4e-5
+        M = 4
+        ntaps = 21
+        s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
+        s = s.resample(fs, beta=beta, renormalise=True)
+        s = impairments.apply_phase_noise(s, lw)
+        s = impairments.apply_PMD_to_field(s, theta, dgd)
+        wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, method=method, adaptive_stepsize=False)
+        sout = equalisation.apply_filter(s, wxy)
+        sout, ph = phaserec.viterbiviterbi(sout,11)
+        sout = helpers.normalise_and_center(sout)
+        sout = helpers.dump_edges(sout, 30)
+        ser = sout.cal_ser()
+        npt.assert_allclose(ser, 0, atol=1.01*3/N)# Three wrong symbols is ok
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize("method", ["cma", "mcma"])
+    @pytest.mark.parametrize("phi", np.linspace(5.9, 7.5, 2))
+    @pytest.mark.parametrize("dgd", [300e-12])
+    @pytest.mark.parametrize("lw", np.linspace(10e3, 1000e3, 2))
+    def test_pmd_phase_fails(self, method, phi, dgd, lw):
+        theta = np.pi/phi
+        fb = 40.e9
+        os = 2
+        fs = os*fb
+        N = 2**16
+        snr = 15
+        beta = 0.3
+        mu = 4e-5
+        M = 4
+        ntaps = 21
+        s = modulation.SignalQAMGrayCoded(M, N, nmodes=2, fb=fb)
+        s = s.resample(fs, beta=beta, renormalise=True)
         s = impairments.apply_phase_noise(s, lw)
         s = impairments.apply_PMD_to_field(s, theta, dgd)
         wxy, err = equalisation.equalise_signal(s, mu, M, Ntaps=ntaps, method=method, adaptive_stepsize=False)
