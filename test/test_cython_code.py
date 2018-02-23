4fc917e90d8b7c785f8655f8aadc2fbd097afb8c
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-23 16:44:22 +0100

    cython quantization test was failing for single precision due to rounding

--- test/test_cython_code.py
+++ test/test_cython_code.py
@@ -2,21 +2,22 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 from dsp import signals, impairments
-from dsp.core.equalisation import equaliser_cython
+from dsp.core.equalisation import cython_equalisation
 
 
 class TestQuantize(object):
-    @pytest.mark.parametrize("dtype", [np.complex64, np.complex128, np.complex256])
+    @pytest.mark.parametrize("dtype", [np.complex64, np.complex128])
     def test_dtype(self, dtype):
         s = signals.SignalQAMGrayCoded(32, 2**12, dtype=dtype)
         s = impairments.change_snr(s, 23).astype(dtype)
-        o = equaliser_cython.quantize(s[0], s.coded_symbols)
+        o = cython_equalisation.quantize(s[0], s.coded_symbols)
         assert o.dtype is np.dtype(dtype)
 
-    @pytest.mark.parametrize("dtype", [np.complex64, np.complex128, np.complex256])
+    @pytest.mark.parametrize("dtype", [np.complex64, np.complex128])
     def test_dtype(self, dtype):
         s = signals.SignalQAMGrayCoded(32, 2**12, dtype=dtype)
         s = impairments.change_snr(s, 22).astype(dtype)
-        o = equaliser_cython.quantize(s[0], s.coded_symbols)
-        npt.assert_array_almost_equal(s.symbols[0], o)
+        o = cython_equalisation.quantize(s[0], s.coded_symbols)
+        xx = abs(s.symbols[0] - o)
+        npt.assert_allclose(xx, 0)
 
