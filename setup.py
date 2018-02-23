01304bfaaac13145b384763aeedbc9ae5a498bd0
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-02-23 21:48:08 +0100

    remove cython import from setuptools it does things automatically

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize
 # To use a consistent encoding
 from codecs import open
 from os import path
