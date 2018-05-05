3b59847e5b2432224d41c829ee5ac0a267ae393a
Jochen Schroeder <jochen.schroeder@gmail.com>
2018-05-05 22:39:10 +0200

    add additional dependencies (thanks to @marbre for reporting)

--- setup.py
+++ setup.py
@@ -123,7 +123,9 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-    },
+        'arrayfire': ["arrayfire"],
+        'numba': ["numba>=0.37.0"],
+    }
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
