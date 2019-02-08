29870a1c11468b9fab44510674c9c77361f4e1a0
Jochen Schroeder <jochen.schroeder@gmail.com>
2019-02-08 21:43:21 +0100

    Include case were sync shift in testcase

--- test/test_ber_functions.py
+++ test/test_ber_functions.py
@@ -94,16 +94,30 @@ class TestSyncAndAdjust(object):
             assert (tx.shape[0] == N) and (rx.shape[0] == N)
 
     @pytest.mark.parametrize("plus", [True, False])
+    @pytest.mark.parametrize("mult", [1, 3])
     @pytest.mark.parametrize("adjust", ['rx', 'tx'])
-    def test_slices(self, plus, adjust):
+    def test_slices(self, plus, mult, adjust):
         x = np.arange(100.)
         xx = np.tile(x, 3)
         if plus:
-            y = xx[11:100+3*11]
+            y = xx[11:100+mult*11]
         else:
-            y = xx[11:100-3*11]
+            y = xx[11:100-mult*11]
         tx, rx = ber_functions.sync_and_adjust(x, y, adjust=adjust)
         npt.assert_array_almost_equal(tx, rx)
+
+    @pytest.mark.parametrize("plus", [True, False])
+    @pytest.mark.parametrize("mult", [1, 3])
+    @pytest.mark.parametrize("adjust", ['rx', 'tx'])
+    def test_slices_length(self, plus, mult, adjust):
+        x = np.arange(100.)
+        xx = np.tile(x, 3)
+        if plus:
+            y = xx[11:100+mult*11]
+        else:
+            y = xx[11:100-mult*11]
+        tx, rx = ber_functions.sync_and_adjust(x, y, adjust=adjust)
+        assert tx.shape == rx.shape
 
     @pytest.mark.parametrize("N0", [(None, 1000), (1000, None)])
     @pytest.mark.parametrize("shiftN", [0, 43, 150, 800])
