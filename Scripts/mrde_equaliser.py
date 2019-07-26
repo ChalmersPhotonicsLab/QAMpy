da0061a936e13393db05aaf9de3f4507f26d1cc5
Jochen Schroeder <jochen.schroeder@gmail.com>
2019-07-26 04:15:48 +0200

    Fix mrde demo script to signal object syntax

--- Scripts/mrde_equaliser.py
+++ Scripts/mrde_equaliser.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
-from qampy import equalisation, signals
-from qampy.core import impairments
+from qampy import equalisation, signals, impairments, helpers
 
 fb = 40.e9
 os = 2
@@ -9,54 +8,57 @@ fs = os*fb
 N = 2**18
 theta = np.pi/2.35
 M = 16
-QAM = signals.QAMModulator(16)
 snr = 24
 muCMA = 1e-3
 muRDE = 0.5e-3
 ntaps = 30
 t_pmd = 50e-12
-Ncma = N//6//os -int(1.5*ntaps)
-Nrde = 5*N//6//os -int(1.5*ntaps)
 
-S, symbols, bits = QAM.generate_signal(N, snr, baudrate=fb, samplingrate=fs, PRBSorder=(15,23))
-SS = impairments.apply_PMD_to_field(S, theta, t_pmd, fs)
+sig = signals.ResampledQAM(M, N, nmodes=2, fb=fb, fs=fs, resamplekwargs={"beta":0.01, "renormalise":True})
+sig = impairments.change_snr(sig, snr)
+SS = impairments.apply_PMD(sig, theta, t_pmd)
 
-E_m, (wx_m, wy_m), (err_m, err_rde_m) = equalisation.dual_mode_equalisation(SS, os, M, ntaps, methods=("mcma", "mrde"),
-                                                                            TrSyms=(Ncma, Nrde))
+E_s, wxy_s, (err_s, err_rde_s) = equalisation.dual_mode_equalisation(SS, (muCMA, muRDE), ntaps,
+                                                                            methods=("mcma", "mrde"))
+E_s = helpers.normalise_and_center(E_s)
+evm = sig[:, ::2].cal_evm()
+evmE_s = E_s.cal_evm()
+gmiE = E_s.cal_gmi()
+print(gmiE)
 
 
-evmX = QAM.cal_evm(S[0, ::2])
-evmY = QAM.cal_evm(S[1, ::2])
-evmEx_m = QAM.cal_evm(E_m[0])
-evmEy_m = QAM.cal_evm(E_m[1])
-#sys.exit()
 plt.figure()
-plt.subplot(121)
+plt.subplot(221)
+plt.hexbin(E_s[0].real, E_s[0].imag)
+plt.text(0.999, 0.9, r"$EVM_x={:.1f}\%$".format(100*evmE_s[0]), color='w', horizontalalignment="right", fontsize=14)
+plt.subplot(222)
 plt.title('Recovered MCMA/MRDE')
-plt.plot(E_m[0].real, E_m[0].imag, 'r.' ,label=r"$EVM_x=%.1f\%%$"%(evmEx_m*100))
-plt.plot(E_m[1].real, E_m[1].imag, 'g.', label=r"$EVM_y=%.1f\%%$"%(100*evmEy_m))
-plt.legend()
-plt.subplot(122)
+plt.hexbin(E_s[1].real, E_s[1].imag)
+plt.text(0.999, 0.9, r"$EVM_y={:.1f}\%$".format(100*evmE_s[1]), color='w', horizontalalignment="right", fontsize=14)
+plt.subplot(223)
 plt.title('Original')
-plt.plot(S[0,::2].real, S[0,::2].imag, 'ro', label=r"$EVM_x=%.1f\%%$"%(100*evmX))
-plt.plot(S[1,::2].real, S[1,::2].imag, 'go', label=r"$EVM_y=%.1f\%%$"%(100*evmY))
-plt.legend()
+plt.hexbin(sig[0,::2].real, sig[0,::2].imag)
+plt.text(0.999, 0.9, r"$EVM_x={:.1f}\%$".format(100*evm[0]), color='w', horizontalalignment="right", fontsize=14)
+plt.subplot(224)
+plt.hexbin(sig[1,::2].real, sig[1,::2].imag)
+plt.text(0.999, 0.9, r"$EVM_y={:.1f}\%$".format(100*evm[1]), color='w', horizontalalignment="right", fontsize=14)
 
 plt.figure()
-plt.subplot(311)
-plt.title('MCMA/MRDE Taps')
-plt.plot(wx_m[0,:], 'r')
-plt.plot(wx_m[1,:], '--r')
-plt.plot(wy_m[0,:], 'g')
-plt.plot(wy_m[1,:], '--g')
-plt.subplot(312)
-plt.title('MCMA/MRDE error cma')
-plt.plot(abs(err_m[0]), color='r')
-plt.plot(abs(err_m[1])- 10, color='g')
-plt.subplot(313)
-plt.title('MCMA/MRDE error rde')
-plt.plot(abs(err_rde_m[0]), color='r')
-plt.plot(abs(err_rde_m[1])-10, color='g')
+plt.subplot(221)
+plt.title('MCMA/MRDE Taps X-Pol')
+plt.plot(wxy_s[0, 0,:], 'r')
+plt.plot(wxy_s[0, 1,:], '--r')
+plt.subplot(223)
+plt.title('MCMA/MRDE Taps Y-Pol')
+plt.plot(wxy_s[1, 0,:], 'g')
+plt.plot(wxy_s[1, 1,:], '--g')
+plt.subplot(222)
+plt.title('MCMA/MRDE error mcma')
+plt.plot(abs(err_s[0]), color='r')
+plt.plot(abs(err_s[1]), color='g')
+plt.subplot(224)
+plt.title('MCMA/MRDE error mrde')
+plt.plot(abs(err_rde_s[0]), color='r')
+plt.plot(abs(err_rde_s[1]), color='g')
 plt.show()
-
 
