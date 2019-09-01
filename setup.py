4c2fa301143d2cd74a2d16983a58e37afe93cdc1
Jochen Schroeder <jochen.schroeder@gmail.com>
2019-09-01 21:24:57 +0200

    Add build dependency for pythran

--- setup.py
+++ setup.py
@@ -137,10 +137,7 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'arrayfire': ["arrayfire"],
-    },
-
+    setup_requiers=["pythran"]
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
