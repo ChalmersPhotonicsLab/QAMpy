5cf6bdef5fa16ae2f9c838becd254c08b06af48b
Jochen Schroeder <jochen.schroeder@gmail.com>
2019-09-01 21:20:42 +0200

    Add pythran build to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages, Extension
+from distutils.core import Extension
+from setuptools import setup, find_packages, dist#, Extension
+#dist.Distribution(dict(setup_requires="pythran"))
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -8,7 +9,10 @@ import numpy as np
 from pythran import PythranExtension
 
 COMPILER_ARGS = ["-O3", "-march=native", "-ffast-math", "-mfpmath=sse", "-funroll-loops", "-fopenmp"]
-COMPILER_ARGS_PYT = ["-O3", "-march=native", "-ffast-math", "-mfpmath=sse", "-funroll-loops", "-DUSE_XSIMD"]
+COMPILER_ARGS_PYT = ["-O3", "-ffast-math", "-mfpmath=sse", "-march=native",
+                     "-funroll-loops",
+                      "-fopenmp", "-std=c++11", "-fno-math-errno", "-w",
+                      "-fvisibility=hidden", "-fno-wrapv", "-DUSE_XSIMD"]
 LINK_ARGS = ["-fopenmp", "-lm"]
 
 WIN_COMPILER_ARGS = ["/O2", "/openmp", "/GL", "/fp:fast", "/arch:AVX2"]
@@ -16,47 +20,47 @@ WIN_LINK_ARGS = ["/openmp"]
 
 if sys.platform.startswith("win"):
     cython_equalisation = Extension(name="qampy.core.equalisation.cython_equalisation",
-                     sources=["qampy/core/equalisation/cython_equalisation.pyx"],
-                             include_dirs=["qampy/core/equalisation", np.get_include()],
-                             language="c++",
-                             extra_compile_args=WIN_COMPILER_ARGS,
-                             extra_link_args=WIN_LINK_ARGS)
+                                    sources=["qampy/core/equalisation/cython_equalisation.pyx"],
+                                    include_dirs=["qampy/core/equalisation", np.get_include()],
+                                    language="c++",
+                                    extra_compile_args=WIN_COMPILER_ARGS,
+                                    extra_link_args=WIN_LINK_ARGS)
     cython_errorfcts = Extension(name="qampy.core.equalisation.cython_errorfcts",
-                     sources=["qampy/core/equalisation/cython_errorfcts.pyx"],
-                             include_dirs=["qampy/core/equalisation", np.get_include()],
-                             language="c++",
-                             extra_compile_args=WIN_COMPILER_ARGS,
-                             extra_link_args=WIN_LINK_ARGS)
+                                 sources=["qampy/core/equalisation/cython_errorfcts.pyx"],
+                                 include_dirs=["qampy/core/equalisation", np.get_include()],
+                                 language="c++",
+                                 extra_compile_args=WIN_COMPILER_ARGS,
+                                 extra_link_args=WIN_LINK_ARGS)
     dsp_cython = Extension(name="qampy.core.dsp_cython",
-                       sources=["qampy/core/dsp_cython.pyx"],
-                             include_dirs=["qampy/core/equalisation", np.get_include(), "qampy/core/"],
+                           sources=["qampy/core/dsp_cython.pyx"],
+                           include_dirs=["qampy/core/equalisation", np.get_include(), "qampy/core/"],
                            language="c++",
-                       extra_compile_args=WIN_COMPILER_ARGS,
-                             extra_link_args=WIN_LINK_ARGS)
+                           extra_compile_args=WIN_COMPILER_ARGS,
+                           extra_link_args=WIN_LINK_ARGS)
 else:
     cython_equalisation = Extension(name="qampy.core.equalisation.cython_equalisation",
-                     sources=["qampy/core/equalisation/cython_equalisation.pyx"],
-                             include_dirs=["qampy/core/equalisation", np.get_include()],
-                             extra_compile_args=COMPILER_ARGS,
-                             extra_link_args=LINK_ARGS)
+                                    sources=["qampy/core/equalisation/cython_equalisation.pyx"],
+                                    include_dirs=["qampy/core/equalisation", np.get_include()],
+                                    extra_compile_args=COMPILER_ARGS,
+                                    extra_link_args=LINK_ARGS)
     cython_errorfcts = Extension(name="qampy.core.equalisation.cython_errorfcts",
-                     sources=["qampy/core/equalisation/cython_errorfcts.pyx"],
-                             include_dirs=["qampy/core/equalisation", np.get_include()],
-                             extra_compile_args=COMPILER_ARGS,
-                             extra_link_args=LINK_ARGS)
+                                 sources=["qampy/core/equalisation/cython_errorfcts.pyx"],
+                                 include_dirs=["qampy/core/equalisation", np.get_include()],
+                                 extra_compile_args=COMPILER_ARGS,
+                                 extra_link_args=LINK_ARGS)
     dsp_cython = Extension(name="qampy.core.dsp_cython",
-                       sources=["qampy/core/dsp_cython.pyx"],
-                             include_dirs=["qampy/core/equalisation", np.get_include(), "qampy/core/"],
-                       extra_compile_args=COMPILER_ARGS,
-                             extra_link_args=LINK_ARGS)
+                           sources=["qampy/core/dsp_cython.pyx"],
+                           include_dirs=["qampy/core/equalisation", np.get_include(), "qampy/core/"],
+                           extra_compile_args=COMPILER_ARGS,
+                           extra_link_args=LINK_ARGS)
     dsp_pythran = PythranExtension(name="qampy.core.pythran_dsp",
                                    sources = ["qampy/core/pythran_dsp.py"],
                                    extra_compile_args=COMPILER_ARGS_PYT,
                                    extra_link_args=LINK_ARGS)
     pythran_equalisation = PythranExtension(name="qampy.core.equalisation.pythran_equalisation",
-                                   sources = ["qampy/core/equalisation/pythran_equalisation.py"],
-                                   extra_compile_args=COMPILER_ARGS_PYT,
-                                   extra_link_args=LINK_ARGS)
+                                            sources = ["qampy/core/equalisation/pythran_equalisation.py"],
+                                            extra_compile_args=COMPILER_ARGS_PYT,
+                                            extra_link_args=LINK_ARGS)
 
 
 
@@ -127,14 +131,14 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy', 'scipy', 'bitarray'],
 
-    ext_modules = [cython_errorfcts, cython_equalisation, dsp_cython, dsp_pythran, pythran_equalisation],
+    #ext_modules = [cython_errorfcts, cython_equalisation, dsp_cython, dsp_pythran, pythran_equalisation],
+    ext_modules = [dsp_pythran, pythran_equalisation],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
         'arrayfire': ["arrayfire"],
-        'numba': ["numba>=0.37.0"],
     },
 
     # If there are data files included in your packages that need to be
@@ -149,3 +153,4 @@ setup(
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[],
 )
+
