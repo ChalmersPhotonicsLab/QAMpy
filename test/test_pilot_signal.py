a3e9e78c5e6e9972bea46b883073e2c29d2bfe35
Jochen Schroeder <jochen.schroeder@gmail.com>
2019-10-02 12:24:27 +0200

    Update test scripts to not do foe compensation

--- test/test_pilot_signal.py
+++ test/test_pilot_signal.py
@@ -14,13 +14,10 @@ class TestPilotSignalRecovery(object):
         sig3 = impairments.change_snr(sig2, snr)
         sig3 = core.impairments.rotate_field(sig3, np.pi*theta)
         sig4 = sig3[:, 20000:]
-        sig4.sync2frame(Ntaps=ntaps)
-        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], ntaps, True)
-        ser = s2.cal_ser(synced=False)
-        snr_m = s2.est_snr(synced=False)
-        snr_db = 10*np.log10(np.mean(snr_m))
+        sig4.sync2frame(corr_coarse_foe=False)
+        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
+        ser = s2.cal_ser()
         assert np.mean(ser) < 1e-4
-        #npt.assert_almost_equal(snr, snr_db)
 
     @pytest.mark.parametrize("theta", np.linspace(0.1, 1, 5))
     @pytest.mark.parametrize("dgd", np.linspace(100e-12, 1000e-12, 5))
@@ -30,16 +27,14 @@ class TestPilotSignalRecovery(object):
         sig = signals.SignalWithPilots(64, 2**16, 1024, 32, nframes=3, nmodes=2, fb=24e9)
         sig2 = sig.resample(2*sig.fb, beta=0.1, renormalise=True)
         sig3 = impairments.change_snr(sig2, snr)
-        sig3 = impairments.apply_PMD(sig3, theta, dgd)
-        #sig3 = core.impairments.rotate_field(sig3, np.pi*theta)
+        sig3 = impairments.apply_PMD(sig3, theta*np.pi, dgd)
         sig4 = sig3[:, 20000:]
-        sig4.sync2frame(Ntaps=ntaps, Niter=10)
-        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], ntaps, True, adaptive_stepsize=True)
+        sig4.sync2frame(corr_coarse_foe=False)
+        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
         ser = s2.cal_ser(synced=False)
         snr_m = s2.est_snr(synced=False)
         snr_db = 10*np.log10(np.mean(snr_m))
         assert np.mean(ser) < 1e-4
-        #npt.assert_almost_equal(snr, snr_db)
 
     def test_swap_pols(self):
         snr = 30.
@@ -48,8 +43,8 @@ class TestPilotSignalRecovery(object):
         sig2 = impairments.change_snr(sig2, snr)
         sig3 = sig2[::-1]
         sig4 = sig3[:, 20000:]
-        sig4.sync2frame()
-        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], 17, True, adaptive_stepsize=True)
+        sig4.sync2frame(corr_coarse_foe=False)
+        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], 17, True, adaptive_stepsize=True, foe_comp=False)
         ser = s2.cal_ser(synced=True)
         assert np.mean(ser) < 1e-4
 
@@ -62,8 +57,8 @@ class TestPilotSignalRecovery(object):
         sig2 = impairments.apply_phase_noise(sig2, 100e3)
         sig3 = impairments.change_snr(sig2, snr)
         sig4 = sig3[:, 20000:]
-        sig4.sync2frame()
-        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], ntaps, True, adaptive_stepsize=True)
+        sig4.sync2frame(corr_coarse_foe=False)
+        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
         d, ph = phaserec.pilot_cpe(s2, nframes=1)
         assert np.mean(d.cal_ber()) < 1e-5
 #
