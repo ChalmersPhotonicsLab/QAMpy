Fix wrong index and left-over arguments

qampy/core/equalisation/equalisation.py
@@ -370,8 +370,8 @@ def dual_mode_equalisation(E, os, mu, M, wxy=None, Ntaps=None, TrSyms=(None,None
     symbols = np.atleast_1d(symbols)
     if symbols.ndim < 3:
         symbols = np.tile(symbols, (2,1,1))
-    wxy, err1 = equalise_signal(E, os, mu[0], M, wxy=wxy, Ntaps=Ntaps, TrSyms=TrSyms[0], Niter=Niter[0], method=methods[0], adaptive_stepsize=adaptive_stepsize[0], symbols=symbols[0], avoid_cma_sing=avoid_cma_sing[0], modes=modes,**kwargs)
-    wxy2, err2 = equalise_signal(E, os, mu[1], M, wxy=wxy, TrSyms=TrSyms[1], Niter=Niter[1], method=methods[1], adaptive_stepsize=adaptive_stepsize[1],  symbols=symbols[0], avoid_cma_sing=avoid_cma_sing[1], modes=modes, **kwargs)
+    wxy, err1 = equalise_signal(E, os, mu[0], M, wxy=wxy, Ntaps=Ntaps, TrSyms=TrSyms[0], Niter=Niter[0], method=methods[0], adaptive_stepsize=adaptive_stepsize[0], symbols=symbols[0],  modes=modes,**kwargs)
+    wxy2, err2 = equalise_signal(E, os, mu[1], M, wxy=wxy, TrSyms=TrSyms[1], Niter=Niter[1], method=methods[1], adaptive_stepsize=adaptive_stepsize[1],  symbols=symbols[1],  modes=modes, **kwargs)
     if apply:
         Eest = apply_filter(E, os, wxy2)
         return Eest, wxy2, (err1, err2)

test/sim_pilot_txrx.py
@@ -2,7 +2,7 @@ import numpy as np
 from qampy.core import equalisation,  phaserecovery, pilotbased_receiver,pilotbased_transmitter,filter,\
     resample
 from qampy import signals, impairments, helpers, phaserec
-from qampy.equalisation import pilot_equalizer
+from qampy.equalisation import pilot_equaliser
 import matplotlib.pylab as plt
 
 def run_pilot_receiver2(rec_signal, process_frame_id=0, foe_comp=True, os=2, M=128, Numtaps=(17, 45),
@@ -14,7 +14,7 @@ def run_pilot_receiver2(rec_signal, process_frame_id=0, foe_comp=True, os=2, M=1
     #signal = np.roll(signal, -shift_factor[shift_factor>=0].min(), axis=-1)
     #shift_factors -= shift_factors[shift_factors>=0].min()
     #signal.shiftfctrs = shift_factors
-    taps_all, eq_mode_sig = pilot_equalizer(rec_signal, mu, Numtaps[1], apply=True, foe_comp=foe_comp, adaptive_stepsize=adap_step[1],
+    taps_all, eq_mode_sig = pilot_equaliser(rec_signal, mu, Numtaps[1], apply=True, foe_comp=foe_comp, adaptive_stepsize=adap_step[1],
                                             Niter=Niter[1], methods=method)
     #symbs, trace = pilotbased_receiver.pilot_based_cpe(eq_mode_sig[:, eq_mode_sig._pilot_seq_len:eq_mode_sig.frame_len],
                                                            #eq_mode_sig.ph_pilots, eq_mode_sig._pilot_ins_rat,

test/test_equalisation.py
@@ -136,7 +136,7 @@ class TestEqualiseSignalParameters(object):
         mysig3 = impairments.simulate_transmission(mysig2,snr=25,dgd=10e-12, freq_off=00e6,lwdth=000e3,roll_frame_sync=rollframe, modal_delay=modal_delay)
         mysig3.sync2frame()
         mysig3.corr_foe()
-        wxy, eq_sig = equalisation.pilot_equalizer(mysig3, (1e-3,1e-3), 45, foe_comp=False, methods=("cma", ddmethod))
+        wxy, eq_sig = equalisation.pilot_equaliser(mysig3, (1e-3, 1e-3), 45, foe_comp=False, methods=("cma", ddmethod))
         cpe_sig, ph = phaserec.pilot_cpe(eq_sig,N=5,use_seq=False) 
         gmi = np.mean(cpe_sig.cal_gmi()[0])
         assert gmi > 5.5

test/test_pilot_signal.py
@@ -15,7 +15,7 @@ class TestPilotSignalRecovery(object):
         sig3 = core.impairments.rotate_field(sig3, np.pi*theta)
         sig4 = sig3[:, 20000:]
         sig4.sync2frame(corr_coarse_foe=False)
-        s1, s2 = equalisation.pilot_equalizer(sig4, [5e-3, 5e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
+        s1, s2 = equalisation.pilot_equaliser(sig4, [5e-3, 5e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
         ph = phaserec.find_pilot_const_phase(s2.extract_pilots()[:,:s2._pilot_seq_len], s2.pilot_seq)
         s2 = phaserec.correct_pilot_const_phase(s2, ph)
         ser = s2.cal_ser()
@@ -32,7 +32,7 @@ class TestPilotSignalRecovery(object):
         sig3 = impairments.apply_PMD(sig3, theta*np.pi, dgd)
         sig4 = sig3[:, 20000:]
         sig4.sync2frame(corr_coarse_foe=False)
-        s1, s2 = equalisation.pilot_equalizer(sig4, [5e-3, 5e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
+        s1, s2 = equalisation.pilot_equaliser(sig4, [5e-3, 5e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
         ph = phaserec.find_pilot_const_phase(s2.extract_pilots()[:,:s2._pilot_seq_len], s2.pilot_seq)
         s2 = phaserec.correct_pilot_const_phase(s2, ph)
         ser = s2.cal_ser(synced=False)
@@ -48,7 +48,7 @@ class TestPilotSignalRecovery(object):
         sig3 = sig2[::-1]
         sig4 = sig3[:, 20000:]
         sig4.sync2frame(corr_coarse_foe=False)
-        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], 17, True, adaptive_stepsize=True, foe_comp=False)
+        s1, s2 = equalisation.pilot_equaliser(sig4, [1e-3, 1e-3], 17, True, adaptive_stepsize=True, foe_comp=False)
         ph = phaserec.find_pilot_const_phase(s2.extract_pilots()[:,:s2._pilot_seq_len], s2.pilot_seq)
         s2 = phaserec.correct_pilot_const_phase(s2, ph)
         ser = s2.cal_ser(synced=True)
@@ -64,7 +64,7 @@ class TestPilotSignalRecovery(object):
         sig3 = impairments.change_snr(sig2, snr)
         sig4 = sig3[:, 20000:]
         sig4.sync2frame(corr_coarse_foe=False)
-        s1, s2 = equalisation.pilot_equalizer(sig4, [1e-3, 1e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
+        s1, s2 = equalisation.pilot_equaliser(sig4, [1e-3, 1e-3], ntaps, True, adaptive_stepsize=True, foe_comp=False)
         d, ph = phaserec.pilot_cpe(s2, nframes=1)
         assert np.mean(d.cal_ber()) < 1e-5
         