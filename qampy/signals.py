31f7a0853286444966ccc0a2bb2c00e8a407081a
Jochen Schroeder <jochen.schroeder@gmail.com>
2020-09-15 22:13:35 +0200

    Add a copy to avoid a pythran error

--- qampy/signals.py
+++ qampy/signals.py
@@ -743,7 +743,7 @@ class SignalQAMGrayCoded(SignalBase):
         coded_symbols, graycode, encoding, bitmap_mtx = cls._generate_mapping(M, scale, dtype=dtype)
         out = np.empty_like(symbs).astype(dtype)
         for i in range(symbs.shape[0]):
-            out[i], _, idx = make_decision(symbs[i], coded_symbols)
+            out[i], _, idx = make_decision(np.copy(symbs[i]), coded_symbols) # need a copy to avoid a pythran error
         bits = cls._demodulate(idx, encoding)
         obj = np.asarray(out).view(cls)
         obj._M = M
