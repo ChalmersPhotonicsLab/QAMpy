1d5c9a09b91e9d966c9c8f2746fc976494326fa8
Jochen Schroeder <jochen.schroeder@gmail.com>
2021-07-01 13:14:07 +0200

    Hack submodule init to display the equalisation methods
    The equalisation methods are not showing their docstrings if they are not given through a comment in the module. This works around that issue.

--- qampy/core/equalisation/__init__.py
+++ qampy/core/equalisation/__init__.py
@@ -1,1 +1,19 @@
-from qampy.core.equalisation.equalisation import equalise_signal, dual_mode_equalisation, apply_filter, DATA_AIDED, REAL_VALUED, DECISION_BASED, NONDECISION_BASED, TRAINING_FCTS
+from qampy.core.equalisation.equalisation import equalise_signal, dual_mode_equalisation, apply_filter, DATA_AIDED, \
+    REAL_VALUED, DECISION_BASED, NONDECISION_BASED, TRAINING_FCTS, __doc__
+
+# the below is a hack to make sphinx pick up the documentation
+
+#: Decision based equalisation methods
+DECISION_BASED = DECISION_BASED
+
+#: Non-decision based equalisation methods
+NONDECISION_BASED = NONDECISION_BASED
+
+#: Real-valued equalisation methods
+REAL_VALUED = REAL_VALUED
+
+#: Data-aided equalisation methods
+DATA_AIDED = DATA_AIDED
+
+#: All available equaliser method#:
+TRAINING_FCTS = TRAINING_FCTS
